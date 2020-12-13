1af8d566f7052e1ba16d8d0fa574e1f461f83586
Mert <mertturkmenoglu99@gmail.com>
2020-12-13 20:45:50 +0300

    Add UserCard unit test

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -3,22 +3,23 @@
     <v-row>
       <v-col cols="4" md="2" sm="2">
         <v-img
-          class="rounded-circle ml-3"
-          :src="user.image"
-          :aspect-ratio="1"
-          elevation="12"
-          alt="User image"
-          width="128"
+            id="user-image"
+            class="rounded-circle ml-3"
+            :src="user.image"
+            :aspect-ratio="1"
+            elevation="12"
+            alt="User image"
+            width="128"
         />
       </v-col>
       <v-col align-self="center" class="ml-3">
         <v-row>
-          <div class="text-h4">
+          <div id="user-name-text" class="text-h4">
             {{ user.name }}
           </div>
         </v-row>
         <v-row>
-          <div class="text-body-1">@{{ user.username }}</div>
+          <div id="username-text" class="text-body-1">@{{ user.username }}</div>
         </v-row>
       </v-col>
     </v-row>
@@ -33,12 +34,12 @@ export default {
 </script>
 
 <style scoped>
-  .v-card {
-    transition: all 0.35s ease-out;
-  }
+.v-card {
+  transition: all 0.35s ease-out;
+}
 
-  .v-card:hover {
-    background-color: #FF7043 !important;
-    color: #fff;
-  }
+.v-card:hover {
+  background-color: #FF7043 !important;
+  color: #fff;
+}
 </style>
---
+++ tests/unit/UserCard.spec.js
@@ -0,0 +1,36 @@
+import { shallowMount } from '@vue/test-utils'
+import UserCard from '@/components/UserCard.vue'
+
+describe('UserCard Unit Tests', () => {
+  let user;
+  let wrapper;
+
+  beforeAll(() => {
+    user = {
+      'name': 'Test User',
+      'username': 'testuser',
+      'image': 'https://example.com/testuser.png'
+    };
+  })
+
+  beforeEach(() => {
+    wrapper = shallowMount(UserCard, {
+      propsData: { user },
+    });
+  })
+
+  it('Renders props.name when passed', () => {
+    const div = wrapper.find('#user-name-text');
+    expect(div.text()).toContain(user.name);
+  })
+
+  it('Renders props.username when passed', () => {
+    const div = wrapper.find('#username-text');
+    expect(div.text()).toContain(user.username);
+  })
+
+  it('Renders props.image when passed', () => {
+    const img = wrapper.find('#user-image')
+    expect(img.attributes().src).toEqual(user.image)
+  })
+})
