8ee4e51c27f47b96d26199f7c3b81d6cb2f7869e
Mert <mertturkmenoglu99@gmail.com>
2021-01-28 17:21:32 +0300

    Remove Recommendations from HomePage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,15 +1,9 @@
 <template>
   <v-container>
-    <v-card class="my-4 recommendation-title hidden-lg-and-up">
-      <h2 class="px-8 py-4 text-center font-weight-light">Recommendations</h2>
+    <v-card class="my-4 title">
+      <h2 class="px-8 py-4 text-center font-weight-light">Your home page is under development.</h2>
+      <h2 class="px-8 py-4 text-center font-weight-light">We'll be back soon.</h2>
     </v-card>
-    <v-row>
-      <v-col v-for="(u, idx) in recommendations[page-1]" :key="idx" cols="12" md="3">
-        <router-link :to="`/user/${u.username}`">
-          <Recommendation :user="u" class=" "/>
-        </router-link>
-      </v-col>
-    </v-row>
     <div v-show="isLoading" class="py-3 text-center">
       <v-progress-circular indeterminate color="deep-orange text--darken-2"/>
     </div>
@@ -20,11 +14,9 @@
 </template>
 
 <script>
-  import Recommendation from "../components/Recommendation";
-
   export default {
     name: "Home",
-    components: {Recommendation},
+    components: {},
     data: () => ({
       user: {},
       recommendations: [],
@@ -39,54 +31,11 @@
         const data = await response.json();
         this.user = data.user[0];
       },
-
-      async fetchRecommendations() {
-        const BASE = "https://vevericka-match-service.herokuapp.com";
-        const URL = `${BASE}/user/${this.username}/recommendations`
-        const response = await fetch(URL);
-        const {matches} = await response.json();
-        this.recommendations = await this.getUsersFromUsernames(matches);
-
-        if (this.recommendations.length > 0) {
-          const arr = [];
-          const pageCount = 4;
-          const itemsPerPage = 8;
-
-          for (let i = 0; i < pageCount; i++) {
-            const tmp = []
-
-            for (let j = 0; j < itemsPerPage; j++) {
-              if (this.recommendations[i * itemsPerPage + j]) {
-                tmp.push(this.recommendations[i * itemsPerPage + j])
-              }
-            }
-
-            arr.push(tmp)
-          }
-
-          this.recommendations = arr;
-        }
-        this.isLoading = false;
-      },
-
-      async getUsersFromUsernames(list) {
-        const BASE = "https://user-info-service.herokuapp.com";
-        const requestOptions = {
-          method: "POST",
-          headers: {"Content-Type": "application/json"},
-          body: JSON.stringify({list}),
-        };
-
-        const URL = `${BASE}/user/get_all_by_username/`;
-        const response = await fetch(URL, requestOptions);
-        const {users} = await response.json();
-
-        return users;
-      },
     },
     mounted() {
-      this.fetchUser();
-      this.fetchRecommendations();
+      this.fetchUser().then(() => {
+        this.isLoading = false;
+      });
     },
     computed: {
       username() {
@@ -97,7 +46,7 @@
 </script>
 
 <style scoped>
-  .recommendation-title {
+  .title {
     background-color: #E64A19;
     color: white;
   }
