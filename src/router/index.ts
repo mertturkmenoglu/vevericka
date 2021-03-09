e9df17166cf9a81262162d717dd452a1c6a94840
Mert <mertturkmenoglu99@gmail.com>
2021-03-09 23:18:26 +0300

    Add lazy loading to Router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,18 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HomePage from '@/views/HomePage.vue'
 import LoginPage from '@/views/LoginPage.vue'
-import RegisterPage from '@/views/RegisterPage.vue'
 import UserPage from '@/views/UserPage.vue'
-import SettingsPage from '@/views/SettingsPage.vue'
-import MessagesPage from '@/views/MessagesPage.vue'
-import SearchPage from '@/views/SearchPage.vue'
-import PasswordResetPage from "../views/PasswordResetPage.vue"
 import PostDetailPage from "@/views/PostDetailPage.vue"
-import ExplorePage from "@/views/ExplorePage.vue"
-import TermsPage from "@/views/TermsPage.vue"
-import BookmarksPage from "@/views/BookmarksPage.vue"
-import NotificationsPage from "@/views/NotificationsPage.vue"
-import ContactPage from "@/views/ContactPage.vue"
 import ReportPage from "@/views/ReportPage.vue"
 
 Vue.use(Router);
@@ -21,21 +11,69 @@ Vue.use(Router);
 export const router = new Router({
     mode: 'history',
     routes: [
-        {path: '/', component: HomePage},
-        {path: '/login', component: LoginPage},
-        {path: '/register', component: RegisterPage},
-        {path: '/password', component: PasswordResetPage},
-        {path: '/user/:username', name: 'UserPage', component: UserPage},
-        {path: '/settings', component: SettingsPage},
-        {path: '/search', component: SearchPage},
-        {path: '/messages', component: MessagesPage},
-        {path: '/post/:id', name: 'PostDetailPage', component: PostDetailPage},
-        {path: '/explore', component: ExplorePage},
-        {path: '/bookmarks', component: BookmarksPage},
-        {path: '/notifications', component: NotificationsPage},
-        {path: '/terms', component: TermsPage},
-        {path: '/contact', component: ContactPage},
-        {path: '/report', name: 'ReportPage', component: ReportPage},
+        {
+            path: '/',
+            component: HomePage
+        },
+        {
+            path: '/login',
+            component: LoginPage
+        },
+        {
+            path: '/register',
+            component: () => import(/* webpackChunkName: "register" */ '../views/RegisterPage.vue')
+        },
+        {
+            path: '/password',
+            component: () => import(/* webpackChunkName: "password_reset" */ '../views/PasswordResetPage.vue')
+        },
+        {
+            path: '/user/:username',
+            name: 'UserPage',
+            component: () => import(/* webpackChunkName: "user" */ '../views/UserPage.vue')
+        },
+        {
+            path: '/settings',
+            component: () => import(/* webpackChunkName: "settings" */ '../views/SettingsPage.vue')
+        },
+        {
+            path: '/search',
+            component: () => import(/* webpackChunkName: "search" */ '../views/SearchPage.vue')
+        },
+        {
+            path: '/messages',
+            component: () => import(/* webpackChunkName: "user" */ '../views/PostDetailPage.vue')
+        },
+        {
+            path: '/post/:id',
+            name: 'PostDetailPage',
+            component: () => import(/* webpackChunkName: "post_detail" */ '../views/PostDetailPage.vue')
+        },
+        {
+            path: '/explore',
+            component: () => import(/* webpackChunkName: "explore" */ '../views/ExplorePage.vue')
+        },
+        {
+            path: '/bookmarks',
+            component: () => import(/* webpackChunkName: "bookmarks" */ '../views/BookmarksPage.vue')
+        },
+        {
+            path: '/notifications',
+            component: () => import(/* webpackChunkName: "notifications" */ '../views/NotificationsPage.vue')
+        },
+        {
+            path: '/terms',
+            component: () => import(/* webpackChunkName: "terms" */ '../views/TermsPage.vue')
+        },
+        {
+            path: '/contact',
+            component: () => import(/* webpackChunkName: "contact" */ '../views/ContactPage.vue')
+        },
+        {
+            path: '/report',
+            name: 'ReportPage',
+            component: () => import(/* webpackChunkName: "report" */ '../views/ReportPage.vue')
+        },
 
         // Otherwise redirect to HomePage
         {path: '*', redirect: '/'}
