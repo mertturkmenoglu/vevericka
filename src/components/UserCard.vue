4179d6050f4befc7dc9f5840811487dc91102dd0
Mert <mertturkmenoglu99@gmail.com>
2021-04-21 21:44:13 +0300

    Update SearchPage layout

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,9 +1,8 @@
 <template>
-  <v-card flat outlined>
+  <v-card flat>
     <v-row>
       <v-col cols="4" xs="2" sm="2" md="2" lg="2">
         <v-img
-            id="user-image"
             class="rounded-circle mx-auto ml-3"
             :src="userImage"
             :aspect-ratio="1"
@@ -14,12 +13,12 @@
       </v-col>
       <v-col align-self="center" class="ml-3">
         <v-row>
-          <div id="user-name-text" class="text-h6 font-weight-light">
+          <div class="text-h6 font-weight-light">
             {{ user.name }}
           </div>
         </v-row>
         <v-row>
-          <div id="username-text" class="text-body-2 font-weight-thin">@{{ user.username }}</div>
+          <div class="deep-orange--text text-body-2 font-weight-thin">@{{ user.username }}</div>
         </v-row>
       </v-col>
     </v-row>
--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -1,26 +1,23 @@
 <template>
   <v-container>
-    <v-card class="mx-auto custom-width" flat outlined>
-      <v-col>
-        <v-text-field
-            v-model="searchStr"
-            append-icon="mdi-magnify"
-            outlined
-            class="pt-5"
-            color="deep-orange text--darken-2"
-            :label="$t('search.search')"
-            type="text"
-            flat
-            solo
-            dense
-            clearable
-            single-line
-            :prefix="prefix"
-            @focusin="prefix = '@'"
-            @focusout="prefix = ''"
-            @keyup.enter.native="search"
-            @click:append="search"/>
-      </v-col>
+    <v-card class="mx-auto custom-width" flat>
+      <v-text-field
+          v-model="searchStr"
+          append-icon="mdi-magnify"
+          outlined
+          color="deep-orange text--darken-2"
+          :label="$t('search.search')"
+          type="text"
+          flat
+          solo
+          dense
+          clearable
+          single-line
+          :prefix="prefix"
+          @focusin="prefix = '@'"
+          @focusout="prefix = ''"
+          @keyup.enter.native="search"
+          @click:append="search"/>
     </v-card>
 
     <div v-show="showLoading" class="py-3 text-center">
@@ -33,14 +30,18 @@
       </v-col>
     </v-container>
 
-    <v-card v-for="result in searchResults" :key="result._id" outlined class="mx-auto my-5 custom-width">
+    <div v-for="(result, idx) in searchResults" :key="result._id" class="mx-auto my-5 custom-width">
       <router-link :to="`/user/${result.username}`">
         <UserCard :user="result" class="card-style"/>
       </router-link>
-    </v-card>
+      <v-divider v-if="idx !== searchResults.length - 1"/>
+    </div>
 
-    <div v-if="searchResults.length === 0">
-      <h1>No users found :(</h1>
+    <div v-if="searchResults.length === 0" class="my-5">
+      <h2 class="text-center font-weight-regular">
+        {{ $t('search.user_not_found')}}
+        <v-icon x-large color="deep-orange">mdi-emoticon-sad-outline</v-icon>
+      </h2>
     </div>
   </v-container>
 </template>
