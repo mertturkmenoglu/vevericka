eab72a90e5c93630946637ae40efefef910190f4
Mert <mertturkmenoglu99@gmail.com>
2021-04-25 20:44:33 +0300

    Fix Auth API Eslint problems

--- src/api/bookmark/index.ts
+++ src/api/bookmark/index.ts
@@ -1,6 +1,6 @@
-import IPost from "@/api/responses/IPost";
-import IBookmark from "@/api/responses/IBookmark";
-import axios from "axios";
+import IPost from '@/api/responses/IPost';
+import IBookmark from '@/api/responses/IBookmark';
+import axios, {AxiosInstance} from 'axios';
 
 type BookmarkDto = {
     postId: string;
@@ -8,31 +8,31 @@ type BookmarkDto = {
 }
 
 class BookmarkService {
-    static get post() {
-        return axios.create({
-            baseURL: process.env.NODE_ENV === 'production'
-                ? 'https://vevericka-backend.herokuapp.com/api/v2/bookmark'
-                : 'http://localhost:5000/api/v2/bookmark',
-            headers: {
-                'authorization': localStorage.getItem('vev-token') || ''
-            }
-        })
-    }
+  static get post(): AxiosInstance {
+    return axios.create({
+      baseURL: process.env.NODE_ENV === 'production'
+        ? 'https://vevericka-backend.herokuapp.com/api/v2/bookmark'
+        : 'http://localhost:5000/api/v2/bookmark',
+      headers: {
+        'authorization': localStorage.getItem('vev-token') || '',
+      },
+    });
+  }
 
-    static async getBookmarksByUsername(username: string): Promise<IPost[]> {
-        const res = await BookmarkService.post.get<IPost[]>('/user/' + username)
-        return res.data
-    }
+  static async getBookmarksByUsername(username: string): Promise<IPost[]> {
+    const res = await BookmarkService.post.get<IPost[]>('/user/' + username);
+    return res.data;
+  }
 
-    static async deleteBookmark(bookmarkId: string): Promise<boolean> {
-        await BookmarkService.post.delete('/' + bookmarkId);
-        return true;
-    }
+  static async deleteBookmark(bookmarkId: string): Promise<boolean> {
+    await BookmarkService.post.delete('/' + bookmarkId);
+    return true;
+  }
 
-    static async createBookmark(bookmark: BookmarkDto): Promise<IBookmark> {
-        const res = await BookmarkService.post.post<IBookmark>('/', bookmark);
-        return res.data
-    }
+  static async createBookmark(bookmark: BookmarkDto): Promise<IBookmark> {
+    const res = await BookmarkService.post.post<IBookmark>('/', bookmark);
+    return res.data;
+  }
 }
 
 export default BookmarkService;
