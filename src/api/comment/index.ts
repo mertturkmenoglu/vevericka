50477a8c6fa0cfadc114a3eaab1c37789f59f8e5
Mert <mertturkmenoglu99@gmail.com>
2021-04-25 20:45:13 +0300

    Fix comment API Eslint problems

--- src/api/comment/index.ts
+++ src/api/comment/index.ts
@@ -1,5 +1,5 @@
-import axios from 'axios'
-import IComment from "@/api/responses/IComment";
+import axios, {AxiosInstance} from 'axios';
+import IComment from '@/api/responses/IComment';
 
 type CommentDto = {
     postId: string;
@@ -8,21 +8,21 @@ type CommentDto = {
 }
 
 class CommentService {
-    static get post() {
-        return axios.create({
-            baseURL: process.env.NODE_ENV === 'production'
-                ? 'https://vevericka-backend.herokuapp.com/api/v2/comment'
-                : 'http://localhost:5000/api/v2/comment',
-            headers: {
-                'authorization': localStorage.getItem('vev-token') || ''
-            }
-        })
-    }
+  static get post(): AxiosInstance {
+    return axios.create({
+      baseURL: process.env.NODE_ENV === 'production'
+        ? 'https://vevericka-backend.herokuapp.com/api/v2/comment'
+        : 'http://localhost:5000/api/v2/comment',
+      headers: {
+        'authorization': localStorage.getItem('vev-token') || '',
+      },
+    });
+  }
 
-    static async createComment(comment: CommentDto): Promise<boolean> {
-        await CommentService.post.post<IComment>('/', comment);
-        return true
-    }
+  static async createComment(comment: CommentDto): Promise<boolean> {
+    await CommentService.post.post<IComment>('/', comment);
+    return true;
+  }
 }
 
-export default CommentService
+export default CommentService;
