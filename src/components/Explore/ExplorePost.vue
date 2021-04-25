47b23ac845d0078facb9f2b3bbc20561da6e99ae
Mert <mertturkmenoglu99@gmail.com>
2021-04-25 20:56:31 +0300

    Fix Explore module components ESLint problems

--- src/components/Explore/ExplorePost.vue
+++ src/components/Explore/ExplorePost.vue
@@ -1,15 +1,22 @@
 <template>
-  <v-card class="px-5" flat>
+  <v-card
+    class="px-5"
+    flat
+  >
     <v-card-title>
       <router-link :to="{ name: 'UserPage', params: { username: post.createdBy.username } }">
-        <v-avatar size="40" class="ml-n3">
+        <v-avatar
+          size="40"
+          class="ml-n3"
+        >
           <v-img
-              class="rounded-circle"
-              :src="userImage"
-              contain
-              width="12"
-              aspect-ratio="1"
-              alt="Profile"/>
+            class="rounded-circle"
+            :src="userImage"
+            contain
+            width="12"
+            aspect-ratio="1"
+            alt="Profile"
+          />
         </v-avatar>
         <span class="hidden-sm-and-down">
           <span class="ml-5 font-weight-regular text--primary">{{ post.createdBy.name }}</span>
@@ -23,30 +30,36 @@
 
     <v-card-text>
       <router-link :to="{ name: 'PostDetailPage', params: { id: post._id } }">
-        <div v-html="makeHTML(post.content)" class="text--darken-2 content text-wrap text--primary px-12">
-        </div>
+        <div
+          class="text--darken-2 content text-wrap text--primary px-12"
+          v-html="makeHTML(post.content)"
+        />
       </router-link>
     </v-card-text>
 
-    <v-card-actions aria-label="Post Actions" class="mr-6">
-      <v-spacer></v-spacer>
+    <v-card-actions
+      aria-label="Post Actions"
+      class="mr-6"
+    >
+      <v-spacer />
       <div class="content-small text--disabled text--primary mr-3">
         {{ (new Date(post.createdAt)).toLocaleDateString() }}
       </div>
       <router-link :to="{ name: 'PostDetailPage', params: { id: post._id } }">
-          <span class="content-small font-weight-light">
-            <v-icon color="deep-orange"> mdi-comment-outline </v-icon>
-            <span class="ml-1 pt-1 text--primary">{{ post.comments.length }}</span>
-          </span>
+        <span class="content-small font-weight-light">
+          <v-icon color="deep-orange"> mdi-comment-outline </v-icon>
+          <span class="ml-1 pt-1 text--primary">{{ post.comments.length }}</span>
+        </span>
       </router-link>
     </v-card-actions>
   </v-card>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import Component from "vue-class-component";
-import {Prop} from "vue-property-decorator";
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import {Prop} from 'vue-property-decorator';
+import {preparePostText} from '@/utils/postUtils';
 
 type ExplorePostType = {
   _id: string;
@@ -56,7 +69,7 @@ type ExplorePostType = {
     name: string;
     username: string;
     image: string;
-  }
+  };
   hashtags: string[];
   mentions: string[];
   content: string;
@@ -67,29 +80,17 @@ type ExplorePostType = {
 @Component({})
 export default class ExplorePost extends Vue {
   @Prop({ required: true }) post!: ExplorePostType
-  readonly URL_REGEX = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/ig
-  readonly USERNAME_REGEX = /@[-A-Z0-9_]+/ig
-  readonly HASHTAG_REGEX = /#[-A-Z0-9_]+/ig
 
-  makeHTML(text: string) {
-    return this.linkify(this.detectHashtags(this.detectUsernames(text)))
-  }
-  detectHashtags(text: string) {
-    return text.replace(this.HASHTAG_REGEX, (h: string) => `<a href="/explore/${h.substr(1)}" class="deep-orange--text">${h}</a>`);
-  }
-  detectUsernames(text: string) {
-    return text.replace(this.USERNAME_REGEX, (u: string) => `<a href="/user/${u.substr(1)}" class="deep-orange--text">${u}</a>`);
-  }
-  linkify(text: string) {
-    return text.replace(this.URL_REGEX, (url: string) => `<a href="${url}" class="deep-orange--text">${url}</a>`);
+  makeHTML(text: string): string {
+    return preparePostText(text);
   }
 
-  get userImage() {
+  get userImage(): string {
     const img = this.post.createdBy.image;
     if (img === 'profile.png') {
-      return '/profile.png'
+      return '/profile.png';
     } else {
-      return img
+      return img;
     }
   }
 }
