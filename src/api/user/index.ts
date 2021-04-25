287b37bf119484c381c2698f3ca35ddce89a4866
Mert <mertturkmenoglu99@gmail.com>
2021-04-25 20:54:37 +0300

    Fix user api Eslint problems

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,6 +1,6 @@
-import axios from 'axios'
-import {IUser} from "@/api/responses/IUser";
-import {IUserSearchResult} from "@/api/responses/IUserSearchResult";
+import axios, {AxiosInstance} from 'axios';
+import IUser from '@/api/responses/IUser';
+import {IUserSearchResult} from '@/api/responses/IUserSearchResult';
 
 type Language = {
     language: string;
@@ -8,8 +8,8 @@ type Language = {
 }
 
 type Location = {
-    city?: string
-    country?: string
+    city?: string;
+    country?: string;
 }
 
 type UpdateUserDto = {
@@ -33,54 +33,54 @@ type UpdateUserDto = {
 
 
 class UserService {
-    static get user() {
-        return axios.create({
-            baseURL: process.env.NODE_ENV === 'production'
-                ? 'https://vevericka-backend.herokuapp.com/api/v2/user'
-                : 'http://localhost:5000/api/v2/user',
-            headers: {
-                'authorization': localStorage.getItem('vev-token') || ''
-            }
-        })
-    }
-    static async updateUser(updateUserDto: UpdateUserDto): Promise<boolean> {
-        await UserService.user.put('/', updateUserDto)
-        return true;
-    }
+  static get user(): AxiosInstance {
+    return axios.create({
+      baseURL: process.env.NODE_ENV === 'production'
+        ? 'https://vevericka-backend.herokuapp.com/api/v2/user'
+        : 'http://localhost:5000/api/v2/user',
+      headers: {
+        'authorization': localStorage.getItem('vev-token') || '',
+      },
+    });
+  }
+  static async updateUser(updateUserDto: UpdateUserDto): Promise<boolean> {
+    await UserService.user.put('/', updateUserDto);
+    return true;
+  }
 
-    static async getUserByUsername(username: string): Promise<IUser> {
-        const res = await UserService.user.get('/username/' + username)
-        return res.data
-    }
+  static async getUserByUsername(username: string): Promise<IUser> {
+    const res = await UserService.user.get('/username/' + username);
+    return res.data;
+  }
 
-    public static async searchByQuery(query: string): Promise<IUserSearchResult[]> {
-        const res = await UserService.user.get<IUserSearchResult[]>('/q', {
-            params: {
-                searchTerm: query
-            }
-        })
-        return res.data
-    }
+  public static async searchByQuery(query: string): Promise<IUserSearchResult[]> {
+    const res = await UserService.user.get<IUserSearchResult[]>('/q', {
+      params: {
+        searchTerm: query,
+      },
+    });
+    return res.data;
+  }
 
-    public static async followUser(thisUsername: string, otherUsername: string): Promise<boolean> {
-        const data = {
-            thisUsername,
-            otherUsername,
-        }
+  public static async followUser(thisUsername: string, otherUsername: string): Promise<boolean> {
+    const data = {
+      thisUsername,
+      otherUsername,
+    };
 
-        await UserService.user.post('/follow', data)
-        return true
-    }
+    await UserService.user.post('/follow', data);
+    return true;
+  }
 
-    public static async unfollowUser(thisUsername: string, otherUsername: string): Promise<boolean> {
-        const data = {
-            thisUsername,
-            otherUsername,
-        }
+  public static async unfollowUser(thisUsername: string, otherUsername: string): Promise<boolean> {
+    const data = {
+      thisUsername,
+      otherUsername,
+    };
 
-        await UserService.user.post('/unfollow', data)
-        return true
-    }
+    await UserService.user.post('/unfollow', data);
+    return true;
+  }
 }
 
-export default UserService
+export default UserService;
