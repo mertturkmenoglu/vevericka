ea67c958e3d4c46f213a46e5d406504f0d8c569b
Mert <mertturkmenoglu99@gmail.com>
2021-04-25 20:46:19 +0300

    Fix explore API Eslint problems

--- src/api/explore/index.ts
+++ src/api/explore/index.ts
@@ -1,5 +1,5 @@
-import axios from "axios";
-import IPost from "@/api/responses/IPost";
+import axios, {AxiosInstance} from 'axios';
+import IPost from '@/api/responses/IPost';
 
 export type Tag = {
     tag: string;
@@ -11,40 +11,40 @@ export type TrendingPerson = {
         name: string;
         username: string;
         image: string;
-    }
+    };
 }
 
 class ExploreService {
-    static get service() {
-        return axios.create({
-            baseURL: process.env.NODE_ENV === 'production'
-                ? 'https://vevericka-backend.herokuapp.com/api/v2/explore'
-                : 'http://localhost:5000/api/v2/explore',
-            headers: {
-                'authorization': localStorage.getItem('vev-token') || ''
-            }
-        })
-    }
+  static get service(): AxiosInstance {
+    return axios.create({
+      baseURL: process.env.NODE_ENV === 'production'
+        ? 'https://vevericka-backend.herokuapp.com/api/v2/explore'
+        : 'http://localhost:5000/api/v2/explore',
+      headers: {
+        'authorization': localStorage.getItem('vev-token') || '',
+      },
+    });
+  }
 
-    static async getTags() {
-        const res = await ExploreService.service.get<Tag[]>('/tags')
-        return res.data
-    }
+  static async getTags(): Promise<Tag[]> {
+    const res = await ExploreService.service.get<Tag[]>('/tags');
+    return res.data;
+  }
 
-    static async getPostsByTag(tag: string) {
-        const res = await ExploreService.service.get<IPost[]>('/tag/' + tag)
-        return res.data
-    }
+  static async getPostsByTag(tag: string): Promise<IPost[]> {
+    const res = await ExploreService.service.get<IPost[]>('/tag/' + tag);
+    return res.data;
+  }
 
-    static async getTrendingPeople() {
-        const res = await ExploreService.service.get<TrendingPerson[]>('/people');
-        return res.data
-    }
+  static async getTrendingPeople(): Promise<TrendingPerson[]> {
+    const res = await ExploreService.service.get<TrendingPerson[]>('/people');
+    return res.data;
+  }
 
-    static async getTrendingPosts() {
-        const res = await ExploreService.service.get<IPost[]>('/posts')
-        return res.data
-    }
+  static async getTrendingPosts(): Promise<IPost[]> {
+    const res = await ExploreService.service.get<IPost[]>('/posts');
+    return res.data;
+  }
 }
 
 export default ExploreService;
