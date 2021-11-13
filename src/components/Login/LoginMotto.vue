60ccf043d8c34a93fdf01e4bd889074ed54a1d15
Mert Türkmenoğlu <mertturkmenoglu99@gmail.com>
2021-11-13 15:31:55 +0300

    Convert LoginMotto to TailwindCSS

--- src/components/Login/LoginMotto.vue
+++ src/components/Login/LoginMotto.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full md:w-8/12 bg-deep-orange flex flex-col justify-center items-center">
-    <v-img max-height="256" max-width="256" src="../../assets/icon_white.svg" />
+    <img src="../../assets/icon_white.svg" alt="Vevericka Logo" class="max-w-xs max-h-64" />
     <h1 class="text-5xl mt-3 text-white">Vevericka</h1>
     <h2 class="overline mt-1 text-white">We are the squirrels who say Vik!</h2>
   </div>
