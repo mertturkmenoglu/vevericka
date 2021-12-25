774be038ca72e0ff0de9a4d5bfb5ae9a39adc46d
Mert Türkmenoğlu <mertturkmenoglu99@gmail.com>
2021-12-25 04:35:41 +0300

    Add stepper to register

--- pages/register.tsx
+++ pages/register.tsx
@@ -7,6 +7,7 @@ import { EyeIcon, EyeOffIcon } from '@heroicons/react/outline';
 import AuthButton from '../components/AuthButton';
 import AuthLink from '../components/AuthLink';
 import { RegisterContext } from '../context/RegisterContext';
+import AuthStepper from '../components/AuthStepper';
 
 const Register: NextPage = () => {
   const ctx = useContext(RegisterContext);
@@ -20,51 +21,64 @@ const Register: NextPage = () => {
 
   return (
     <AuthLayout pageTitle="Register | Vevericka" formTitle="Register">
-      <AuthForm>
-        <AuthInputField
-          label="Email"
-          placeholder="Email"
-          type="email"
-          update={ctx.setEmail}
-        />
+      <AuthStepper>
+        <AuthStepper.Step>
+          <AuthForm>
+            <AuthInputField
+              label="Email"
+              placeholder="Email"
+              type="email"
+              update={ctx.setEmail}
+            />
 
-        <AuthInputField
-          label="Username"
-          placeholder="Username"
-          type="text"
-          update={ctx.setUsername}
-        />
+            <AuthInputField
+              label="Username"
+              placeholder="Username"
+              type="text"
+              update={ctx.setUsername}
+            />
+          </AuthForm>
+        </AuthStepper.Step>
 
-        <AuthInputField
-          label="Name"
-          placeholder="Name"
-          type="text"
-          update={ctx.setName}
-        />
+        <AuthStepper.Step>
+          <AuthForm>
+            <AuthInputField
+              label="Name"
+              placeholder="Name"
+              type="text"
+              update={ctx.setName}
+            />
 
-        <AuthInputField
-          label="Password"
-          placeholder="Password"
-          type={ctx.showPassword ? 'text' : 'password'}
-          update={ctx.setPassword}
-          appendIcon={() => {
-            return ctx.showPassword ? (
-              <EyeIcon className="w-5 h-5" />
-            ) : (
-              <EyeOffIcon className="w-5 h-5" />
-            );
-          }}
-          appendIconAlt="Show password"
-          appendIconClick={onShowPasswordClick}
-        />
+            <AuthInputField
+              label="Password"
+              placeholder="Password"
+              type={ctx.showPassword ? 'text' : 'password'}
+              update={ctx.setPassword}
+              appendIcon={() => {
+                return ctx.showPassword ? (
+                  <EyeIcon className="w-5 h-5" />
+                ) : (
+                  <EyeOffIcon className="w-5 h-5" />
+                );
+              }}
+              appendIconAlt="Show password"
+              appendIconClick={onShowPasswordClick}
+            />
 
-        <AuthButton text="Login" onClick={() => {}} />
+            <AuthButton
+              text="Register"
+              onClick={(e) => {
+                e.preventDefault();
+              }}
+            />
+          </AuthForm>
+        </AuthStepper.Step>
+      </AuthStepper>
 
-        <div className="mt-4 flex flex-col text-gray-600 text-sm w-full items-center">
-          <AuthLink href="/login" text="Already have an account?" cta="Login" />
-          <AuthLink href="/reset" text="Forgot password?" cta="Reset" />
-        </div>
-      </AuthForm>
+      <div className="mt-4 flex flex-col text-gray-600 text-sm w-full items-center">
+        <AuthLink href="/login" text="Already have an account?" cta="Login" />
+        <AuthLink href="/reset" text="Forgot password?" cta="Reset" />
+      </div>
     </AuthLayout>
   );
 };
