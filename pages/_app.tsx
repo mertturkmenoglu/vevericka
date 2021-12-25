82cbf02c1f8b42bc6d02c84bede71b0abec09886
Mert Türkmenoğlu <mertturkmenoglu99@gmail.com>
2021-12-25 03:46:19 +0300

    Add RegisterContextProvider to App file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import LoginContextProvider from '../context/LoginContextProvider';
+import RegisterContextProvider from '../context/RegisterContextProvider';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <LoginContextProvider>
-      <Component {...pageProps} />
+      <RegisterContextProvider>
+        <Component {...pageProps} />
+      </RegisterContextProvider>
     </LoginContextProvider>
   );
 };
