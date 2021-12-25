1d8f9f2d6516482a9a7b55dd6d585dea927567c4
Mert Türkmenoğlu <mertturkmenoglu99@gmail.com>
2021-12-25 04:35:33 +0300

    Add AuthStepper component

---
+++ components/AuthStepper/index.tsx
@@ -0,0 +1,76 @@
+import { useState, Children, useEffect, useMemo } from 'react';
+import clsx from 'clsx';
+import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/outline';
+import AuthStepperStep, { AuthStepperStepProps } from './AuthStepperStep';
+
+export interface AuthStepperProps {}
+
+type Component = React.FC<AuthStepperProps> & {
+  Step: React.FC<AuthStepperStepProps>;
+};
+
+const AuthStepper: Component = ({ children }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [safeRenderFlag, setSafeRenderFlag] = useState(false);
+  const childrenArray = Children.toArray(children);
+
+  /**
+   * Check childrens
+   * @returns true if index boundaries are safe
+   * @returns false if index is not safe to use
+   */
+  const checkChildrenArrayIndexBound = () => {
+    if (childrenArray.length === 0) {
+      return false;
+    }
+
+    return true;
+  };
+
+  useEffect(() => {
+    setSafeRenderFlag(checkChildrenArrayIndexBound());
+  }, [childrenArray]);
+
+  const isFirstStep = useMemo(() => {
+    return currentIndex === 0;
+  }, [currentIndex]);
+
+  const isLastStep = useMemo(() => {
+    return currentIndex === childrenArray.length - 1;
+  }, [currentIndex, childrenArray]);
+
+  if (!safeRenderFlag) {
+    return <></>;
+  }
+
+  return (
+    <div className="flex flex-col w-full">
+      {childrenArray[currentIndex]}
+
+      {/* Navigation buttons */}
+      <div className="mt-8 w-full flex justify-between">
+        <button
+          onClick={() => setCurrentIndex((prev) => prev - 1)}
+          className={clsx('flex items-center', { 'opacity-0': isFirstStep })}
+          disabled={isFirstStep}
+        >
+          <ArrowLeftIcon className="w-5 h-5" />
+          <span className="ml-2">Previous</span>
+        </button>
+
+        <button
+          onClick={() => setCurrentIndex((prev) => prev + 1)}
+          className={clsx('flex items-center', { 'opacity-0': isLastStep })}
+          disabled={isLastStep}
+        >
+          <span>Next</span>
+          <ArrowRightIcon className="w-5 h-5 ml-2" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+AuthStepper.Step = AuthStepperStep;
+
+export default AuthStepper;
