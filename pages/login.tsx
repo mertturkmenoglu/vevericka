8e6738be3370b5a1fecff215edefdabf40fb4907
Mert Türkmenoğlu <mertturkmenoglu99@gmail.com>
2021-12-25 03:44:19 +0300

    Remove LoginContextProvider from login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -7,7 +7,6 @@ import AuthButton from '../components/AuthButton';
 import AuthInputField from '../components/AuthInputField';
 import AuthForm from '../components/AuthForm';
 import { LoginContext } from '../context/LoginContext';
-import LoginContextProvider from '../context/LoginContextProvider';
 
 const Login: NextPage = () => {
   const ctx = useContext(LoginContext);
@@ -20,41 +19,44 @@ const Login: NextPage = () => {
   };
 
   return (
-    <LoginContextProvider>
-      <AuthLayout pageTitle="Login | Vevericka" formTitle="Login">
-        <AuthForm>
-          <AuthInputField
-            label="Email"
-            placeholder="Email"
-            type="email"
-            update={ctx.setEmail}
-          />
+    <AuthLayout pageTitle="Login | Vevericka" formTitle="Login">
+      <AuthForm>
+        <AuthInputField
+          label="Email"
+          placeholder="Email"
+          type="email"
+          update={ctx.setEmail}
+        />
 
-          <AuthInputField
-            label="Password"
-            placeholder="Password"
-            type={ctx.showPassword ? 'text' : 'password'}
-            update={ctx.setPassword}
-            appendIcon={() => {
-              return ctx.showPassword ? (
-                <EyeIcon className="w-5 h-5" />
-              ) : (
-                <EyeOffIcon className="w-5 h-5" />
-              );
-            }}
-            appendIconAlt="Show password"
-            appendIconClick={onShowPasswordClick}
-          />
+        <AuthInputField
+          label="Password"
+          placeholder="Password"
+          type={ctx.showPassword ? 'text' : 'password'}
+          update={ctx.setPassword}
+          appendIcon={() => {
+            return ctx.showPassword ? (
+              <EyeIcon className="w-5 h-5" />
+            ) : (
+              <EyeOffIcon className="w-5 h-5" />
+            );
+          }}
+          appendIconAlt="Show password"
+          appendIconClick={onShowPasswordClick}
+        />
 
-          <AuthButton text="Login" onClick={() => {}} />
+        <AuthButton
+          text="Login"
+          onClick={(e) => {
+            e.preventDefault();
+          }}
+        />
 
-          <div className="mt-4 flex flex-col text-gray-600 text-sm w-full items-center">
-            <AuthLink href="/register" text="New user?" cta="Register" />
-            <AuthLink href="/reset" text="Forgot password?" cta="Reset" />
-          </div>
-        </AuthForm>
-      </AuthLayout>
-    </LoginContextProvider>
+        <div className="mt-4 flex flex-col text-gray-600 text-sm w-full items-center">
+          <AuthLink href="/register" text="New user?" cta="Register" />
+          <AuthLink href="/reset" text="Forgot password?" cta="Reset" />
+        </div>
+      </AuthForm>
+    </AuthLayout>
   );
 };
 
