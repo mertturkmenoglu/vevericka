e9127f547d9bd49c4467a8c4d638be7ae742cb6f
Mert Türkmenoğlu <mertturkmenoglu99@gmail.com>
2021-12-26 11:55:41 +0300

    Add ApplicationContextProvider to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,20 @@ import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import LoginContextProvider from '../context/LoginContextProvider';
 import RegisterContextProvider from '../context/RegisterContextProvider';
+import ApplicationContextProvider from '../context/ApplicationContextProvider';
 import { SessionProvider } from 'next-auth/react';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <SessionProvider session={pageProps.session}>
-      <LoginContextProvider>
-        <RegisterContextProvider>
-          <Component {...pageProps} />
-        </RegisterContextProvider>
-      </LoginContextProvider>
-    </SessionProvider>
+    <ApplicationContextProvider>
+      <SessionProvider session={pageProps.session}>
+        <LoginContextProvider>
+          <RegisterContextProvider>
+            <Component {...pageProps} />
+          </RegisterContextProvider>
+        </LoginContextProvider>
+      </SessionProvider>
+    </ApplicationContextProvider>
   );
 };
 
