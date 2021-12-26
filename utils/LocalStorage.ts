b1f3c12b4ef57c36fa21112d9e8c787d73ad8d0e
Mert Türkmenoğlu <mertturkmenoglu99@gmail.com>
2021-12-26 11:54:59 +0300

    Add theme local storage utility

--- utils/LocalStorage.ts
+++ utils/LocalStorage.ts
@@ -1,6 +1,7 @@
 export type LocalStorageField =
   | 'v-token'
-  | 'v-user';
+  | 'v-user'
+  | 'v-is-dark-theme';
 
 export class LocalStorage {
   private setValue(field: LocalStorageField, value: string): void {
@@ -17,5 +18,20 @@ export class LocalStorage {
 
   public set vToken(value: string) {
     this.setValue('v-token', value);
+  }
+
+  public get isDarkTheme(): boolean {
+    const strValue = this.getValue('v-is-dark-theme');
+
+    if (!strValue) {
+      this.setValue('v-is-dark-theme', 'false');
+      return false;
+    }
+
+    return strValue === 'true';
+  }
+
+  public set isDarkTheme(value: boolean) {
+    this.setValue('v-is-dark-theme', value.toString());
   }
 }
