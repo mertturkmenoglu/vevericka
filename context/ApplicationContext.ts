e62272351cdc06bb2d7ff6c9eaac36c6c760779e
Mert Türkmenoğlu <mertturkmenoglu99@gmail.com>
2021-12-26 11:55:11 +0300

    Add application context

---
+++ context/ApplicationContext.ts
@@ -0,0 +1,38 @@
+import { createContext } from "react";
+import { SetState } from "./types";
+
+export interface IApplicationContextState {
+  user: {
+    email: string;
+    setEmail: SetState<string>;
+    username: string;
+    setUsername: SetState<string>;
+    userId: string;
+    setUserId: SetState<string>;
+    image: string;
+    setImage: SetState<string>;
+    name: string;
+    setName: SetState<string>;
+  };
+  isDarkTheme: boolean;
+  setIsDarkTheme: SetState<boolean>;
+}
+
+export const defaultApplicationContextState: IApplicationContextState = {
+  user: {
+    email: '',
+    username: '',
+    userId: '',
+    image: '',
+    name: '',
+    setEmail: () => { },
+    setUsername: () => { },
+    setUserId: () => { },
+    setImage: () => { },
+    setName: () => { },
+  },
+  isDarkTheme: false,
+  setIsDarkTheme: () => { },
+};
+
+export const ApplicationContext = createContext<IApplicationContextState>(defaultApplicationContextState);
---
+++ context/ApplicationContextProvider.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { LocalStorage } from '../utils/LocalStorage';
+import {
+  ApplicationContext,
+  defaultApplicationContextState as defaultValues,
+} from './ApplicationContext';
+
+export interface ApplicationContextProviderProps {}
+
+const ApplicationContextProvider: React.FC<ApplicationContextProviderProps> = ({
+  children,
+}) => {
+  const [email, setEmail] = useState(defaultValues.user.email);
+  const [username, setUsername] = useState(defaultValues.user.username);
+  const [userId, setUserId] = useState(defaultValues.user.userId);
+  const [image, setImage] = useState(defaultValues.user.image);
+  const [name, setName] = useState(defaultValues.user.name);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  return (
+    <ApplicationContext.Provider
+      value={{
+        user: {
+          email,
+          username,
+          userId,
+          image,
+          name,
+          setEmail,
+          setUsername,
+          setUserId,
+          setImage,
+          setName,
+        },
+        isDarkTheme,
+        setIsDarkTheme,
+      }}
+    >
+      {children}
+    </ApplicationContext.Provider>
+  );
+};
+
+export default ApplicationContextProvider;
