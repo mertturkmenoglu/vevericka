739deee0795c37193f6099c9b1fa53c66c954e9c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-01-29 17:52:37 +0300

    Update Tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,16 @@ const colors = require('tailwindcss/colors');
 module.exports = {
   darkMode: 'class',
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     colors: {
       ...colors,
-      'deep-orange': '#ff5722',
-      'dark-primary': '#262626',
+      'primary': '#be185d',
+      'midnight': '#1a1a1a',
     },
     extend: {},
   },
   plugins: [],
-}
+};
