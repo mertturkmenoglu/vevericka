f1315b4f2baf050590a64c8e2b1420be078dbc45
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-01-31 00:55:06 +0300

    Update latest playlist page and add OG tags

--- pages/blog/playlist/latest.tsx
+++ pages/blog/playlist/latest.tsx
@@ -1,5 +1,8 @@
+import { useEffect } from 'react';
 import type { NextPage } from 'next';
 import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
 import { Blog } from '../../../backend/Blog';
 import { IPlaylist } from '../../../backend/models/IPlaylist';
 
@@ -10,11 +13,63 @@ export interface LatestPlaylistPageProps {
 const LatestPlaylistPage: NextPage<LatestPlaylistPageProps> = ({
   playlist,
 }) => {
+  const styles =
+    'first-letter:text-7xl first-letter:font-bold first-letter:text-primary first-letter:mr-3 first-letter:float-left sm:w-2/3 list-disc ml-2 sm:ml-16 my-16  ';
+  useEffect(() => {
+    document.querySelector('body')?.classList?.add('bg-gray-50');
+  });
+
   return (
     <>
-      <h1>{playlist.title}</h1>
-      <h2>{playlist.subtitle}</h2>
-      <div dangerouslySetInnerHTML={{ __html: playlist.content }} />
+      <Head>
+        <title>{playlist.title}</title>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,viewport-fit=cover"
+        />
+        <meta name="title" content={playlist.title} />
+        <meta property="og:site_name" content="Vevericka" />
+        <meta property="og:title" content={playlist.title} />
+        <meta property="og:description" content={playlist.subtitle} />
+        <meta
+          property="og:image"
+          content={playlist.thumbnail.fields.file.url}
+        />
+        <meta
+          property="og:url"
+          content="https://vevericka.app/blog/playlist/latest"
+        />
+        <meta property="og:type" content="article" />
+        <meta property="og:locale" content="en_US" />
+      </Head>
+      <div id="inject-styles" className={`hidden ${styles}`} />
+      <main className="mt-16 mx-8 sm:mx-auto sm:container">
+        <Link href="/">
+          <a className="underline text-sm font-medium tracking-tighter text-midnight">
+            Go to home
+          </a>
+        </Link>
+        <div className="flex flex-col items-center pb-4 border-midnight border-b-2 sm:flex-row">
+          <img
+            src={playlist.thumbnail.fields.file.url}
+            alt={playlist.thumbnail.fields.description}
+            className="mt-8 rounded-full h-48 w-48 sm:h-32 sm:w-32"
+          />
+          <div className="sm:ml-8">
+            <h1 className="text-2xl font-bold font-lato mt-8 text-midnight underline underline-offset-4 text-center sm:text-left">
+              {playlist.title}
+            </h1>
+            <h2 className="font-lato mt-4 text-midnight text-center sm:text-left sm:mt-2">
+              {playlist.subtitle}
+            </h2>
+          </div>
+        </div>
+
+        <div
+          dangerouslySetInnerHTML={{ __html: playlist.content }}
+          className="mt-8"
+        />
+      </main>
     </>
   );
 };
