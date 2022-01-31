b61b8b9cd0aad757e4fbd8e1fd96115153424b12
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-01-31 23:02:43 +0300

    Update AuthLink styles

--- components/AuthLink.tsx
+++ components/AuthLink.tsx
@@ -10,7 +10,7 @@ const AuthLink = ({ href, text, cta }: AuthLinkProps) => {
   return (
     <Link href={href}>
       <a>
-        <span className="text-slate-700 dark:text-gray-400">{text}</span>{' '}
+        <span className="text-midnight dark:text-gray-400">{text}</span>{' '}
         <span className="text-primary hover:underline">{cta}</span>
       </a>
     </Link>
