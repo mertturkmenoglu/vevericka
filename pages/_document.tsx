d2c87cf262233c7b9d3cfd31ea12fec944708f07
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-02-02 23:14:27 +0300

    Add new favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link
+            rel="icon"
+            type="image/svg+xml"
+            href="/assets/images/favicon.svg"
+          />
+          <link rel="icon" type="image/png" href="/assets/images/favicon.png" />
+        </Head>
         <body className="dark:bg-neutral-900">
           <Main />
           <NextScript />
