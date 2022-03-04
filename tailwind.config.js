360d4484c069e64f427480fcb06cdc98f8cfd645
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-03-04 22:17:29 +0300

    Add atom folder to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,12 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
   darkMode: 'class',
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './atom/**/*.{js,ts,jsx,tsx}'],
   theme: {
     colors: {
       ...colors,
-      'primary': '#be185d',
-      'midnight': '#1a1a1a',
+      primary: '#be185d',
+      midnight: '#1a1a1a',
     },
     extend: {},
   },
