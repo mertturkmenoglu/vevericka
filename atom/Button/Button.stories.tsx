22ece55aa27af1367ba6cef7f57d76f9465c65ab
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-03-04 22:18:30 +0300

    Initialize Button component

--- atom/Button/Button.stories.tsx
+++ atom/Button/Button.stories.tsx
@@ -2,6 +2,7 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import * as React from 'react';
 
 import Button from './Button';
+import { DownloadIcon } from '@heroicons/react/outline';
 
 export default {
   title: 'Atom/Button',
@@ -13,4 +14,19 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Default = Template.bind({});
 Default.args = {
   text: 'Default Button',
+  appearance: 'primary',
+};
+
+export const WithPrependIcon = Template.bind({});
+WithPrependIcon.args = {
+  text: 'Download',
+  appearance: 'primary',
+  prependIcon: <DownloadIcon className="ml-2 h-4 w-4" />,
+};
+
+export const WithAppendIcon = Template.bind({});
+WithAppendIcon.args = {
+  text: 'Download',
+  appearance: 'primary',
+  appendIcon: <DownloadIcon className="mr-2 h-4 w-4" />,
 };
--- atom/Button/Button.tsx
+++ atom/Button/Button.tsx
@@ -1,9 +1,76 @@
-export interface ButtonProps {
-  text: string;
+import { ButtonHTMLAttributes, useMemo } from 'react';
+import clsx from 'clsx';
+import type { AppearanceType, SpacingType } from '../common/';
+import { appearanceMapping, spacingMapping } from './Button.variants';
+import { SpinnerAppearance } from '../Spinner';
+import Spinner from '../Spinner/Spinner';
+
+export interface ButtonAtomicProps {
+  appearance: AppearanceType;
+  text?: string | undefined;
+  loading?: boolean | undefined;
+  rounded?: boolean | undefined;
+  spacing?: SpacingType | undefined;
+  block?: boolean | undefined;
+  appendIcon?: JSX.Element | undefined;
+  prependIcon?: JSX.Element | undefined;
 }
 
-const Button: React.FC<ButtonProps> = ({ text }) => {
-  return <button className="bg-midnight text-white">{text}</button>;
+export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & ButtonAtomicProps;
+
+const Button: React.FC<ButtonProps> = ({
+  appearance,
+  text,
+  loading = false,
+  rounded = false,
+  spacing = 'small',
+  block = false,
+  appendIcon,
+  prependIcon,
+  ...rest
+}) => {
+  const calcSpacing = useMemo(() => {
+    return spacingMapping[spacing];
+  }, [spacing]);
+
+  const calcAppearance = useMemo(() => {
+    return clsx(appearanceMapping[appearance], 'hover:transform hover:scale-[1.01]');
+  }, [appearance]);
+
+  const calcBlock = useMemo(() => {
+    return clsx({
+      'w-full': block,
+    });
+  }, [block]);
+
+  const loadingAppearance = useMemo(() => {
+    const map: Record<AppearanceType, SpinnerAppearance> = {
+      primary: 'white',
+      warning: 'white',
+      subtle: 'primary',
+      danger: 'white',
+      accent: 'white',
+    };
+
+    return map[appearance];
+  }, [appearance]);
+
+  const showPrependIcon = useMemo(() => prependIcon !== undefined && !loading, [prependIcon, loading]);
+
+  const showAppendIcon = useMemo(() => appendIcon !== undefined && !loading, [appendIcon, loading]);
+
+  const className = clsx('flex items-center justify-center', calcSpacing, calcAppearance, calcBlock, {
+    'rounded-full': rounded,
+  });
+
+  return (
+    <button className={className} {...rest}>
+      {showAppendIcon && appendIcon}
+      {text !== undefined && !loading && text}
+      {loading && <Spinner appearance={loadingAppearance} spacing={spacing} />}
+      {showPrependIcon && prependIcon}
+    </button>
+  );
 };
 
 export default Button;
