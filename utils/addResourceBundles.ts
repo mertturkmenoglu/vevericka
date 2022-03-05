3a5a0091dd7a5faea2d5a753fa6f1d2a93dbe944
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-03-05 22:37:34 +0300

    Reformat addResourceBundles.ts file

--- utils/addResourceBundles.ts
+++ utils/addResourceBundles.ts
@@ -1,11 +1,7 @@
 import { i18n as i18nType } from 'next-i18next';
 import { AvailableLanguageKey, availableLanguages } from './AvailableLanguages';
 
-export function addResourceBundles(
-  i18n: typeof i18nType,
-  ns: string,
-  resources: Record<AvailableLanguageKey, any>
-) {
+export function addResourceBundles(i18n: typeof i18nType, ns: string, resources: Record<AvailableLanguageKey, any>) {
   if (!i18n) {
     throw Error('i18n object is null');
   }
