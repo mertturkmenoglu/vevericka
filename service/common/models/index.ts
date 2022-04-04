b6569b82b37358be3b4e36b68afdf4b01ac1ff90
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-04-05 01:30:36 +0300

    Update common models barrel file

--- service/common/models/index.ts
+++ service/common/models/index.ts
@@ -2,3 +2,6 @@ export * from './Post';
 export * from './Tag';
 export * from './SinglePost';
 export * from './LikeStatus';
+export * from './MinimalUserResponse';
+export * from './PostImage';
+export * from './PostVideo';
