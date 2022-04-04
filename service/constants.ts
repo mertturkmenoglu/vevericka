4717d3e063619f558d8bc55d49f4d64126460cc3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-04-04 16:28:45 +0300

    Update Api constants

--- service/constants.ts
+++ service/constants.ts
@@ -1,5 +1,4 @@
 // export const BASE_URL = 'https://vevericka-backend.herokuapp.com';
-
-export const BASE_URL = 'http://localhost:5001';
+export const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://api.vevericka.app' : 'http://localhost:5001';
 
 export const API_VER = 'v3';
