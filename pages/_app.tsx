b01eaa2188053f7da6825163ea2410264a1d74b7
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-04-04 16:00:11 +0300

    Add Algolia and React Query providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,19 +9,32 @@ import ApplicationContextProvider from '../context/ApplicationContextProvider';
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 import { ToastContainer } from 'react-toastify';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import algoliasearch from 'algoliasearch/lite';
+import { InstantSearch } from 'react-instantsearch-hooks';
+
+const queryClient = new QueryClient();
+const searchClient = algoliasearch(
+  process.env.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID,
+  process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_ONLY_API_KEY,
+);
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ApplicationContextProvider>
       <ThemeProvider attribute="class">
-        <SessionProvider session={pageProps.session}>
-          <LoginContextProvider>
-            <RegisterContextProvider>
-              <Component {...pageProps} />
-              <ToastContainer />
-            </RegisterContextProvider>
-          </LoginContextProvider>
-        </SessionProvider>
+        <QueryClientProvider client={queryClient}>
+          <SessionProvider session={pageProps.session}>
+            <InstantSearch searchClient={searchClient} indexName="USER">
+              <LoginContextProvider>
+                <RegisterContextProvider>
+                  <Component {...pageProps} />
+                  <ToastContainer />
+                </RegisterContextProvider>
+              </LoginContextProvider>
+            </InstantSearch>
+          </SessionProvider>
+        </QueryClientProvider>
       </ThemeProvider>
     </ApplicationContextProvider>
   );
