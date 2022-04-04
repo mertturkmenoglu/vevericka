f63da20b0fe27d338c340dedbba23de323591d74
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-04-05 01:30:50 +0300

    Update FeedPost model

--- service/common/models/FeedPost.ts
+++ service/common/models/FeedPost.ts
@@ -1,14 +1,10 @@
+import { LikeStatus } from './LikeStatus';
+import { MinimalUserResponse } from './MinimalUserResponse';
 import { Post } from './Post';
 
 export type FeedPost = Post & {
-  user: {
-    id: number;
-    username: string;
-    name: string;
-    image: string;
-    verified: boolean;
-    protected: boolean;
-  };
+  user: MinimalUserResponse;
+  likeStatus: LikeStatus;
   _count: {
     likes: number;
     dislikes: number;
