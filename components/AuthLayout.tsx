e7878d9a3e5c9906cae7cb439316334362231aa3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-04-10 14:04:44 +0300

    Add go to home page link to AuthLayout

--- components/AuthLayout.tsx
+++ components/AuthLayout.tsx
@@ -1,6 +1,8 @@
+import { ArrowLeftIcon } from '@heroicons/react/solid';
 import { useTranslation } from 'next-i18next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export interface AuthLayoutProps {
   pageTitle: string;
@@ -17,7 +19,13 @@ const AuthLayout = ({ pageTitle, formTitle, children }: AuthLayoutProps) => {
         <title>{pageTitle}</title>
       </Head>
 
-      <div className="flex h-1/2 w-full flex-col items-center justify-center bg-midnight text-white lg:h-full lg:w-8/12">
+      <div className="relative flex h-1/2 w-full flex-col items-center justify-center bg-midnight text-white lg:h-full lg:w-8/12">
+        <Link href="/">
+          <a className="absolute top-4 left-4 flex items-center justify-center hover:underline">
+            <ArrowLeftIcon className="h-6 w-6 text-white" />
+            <span className="ml-2">{t('goHome')}</span>
+          </a>
+        </Link>
         <Image src="/assets/icon_white.svg" width={256} height={256} alt={t('logoAlt')} />
         <h1 className="mt-8 text-6xl font-thin">{t('appName')}</h1>
         <h2 className="mt-4 text-center font-medium uppercase tracking-wide">{t('subtitle')}</h2>
