93e48f43de781c1bc180c398d0be56b27a471ec9
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-04-11 14:27:30 +0300

    Update PostCard title user name styles

--- components/PostCard/PostCardTitle.tsx
+++ components/PostCard/PostCardTitle.tsx
@@ -58,7 +58,7 @@ const PostCardTitle: React.FC<PostCardTitleProps> = ({ post }) => {
         </Link>
         <div className="ml-2 flex flex-col justify-center">
           <Link href={`/user/${post.user.username}`}>
-            <a className="flex items-center text-xl text-midnight hover:underline dark:text-gray-200">
+            <a className="flex items-center text-base text-midnight hover:underline dark:text-gray-200">
               <span>{post.user.name}</span>
               {post.user.verified && <BadgeCheckIcon className="ml-1 mt-1 h-4 w-4 text-primary" />}
             </a>
