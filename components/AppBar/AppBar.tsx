1af469ff0bcc6e062abeeda774cd3b317ed3c008
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-04-11 14:27:52 +0300

    Change AppBar width and add control prop

--- components/AppBar/AppBar.tsx
+++ components/AppBar/AppBar.tsx
@@ -2,12 +2,23 @@ import AppBarMenu from './elements/AppBarMenu';
 import AppBarSearch from './elements/AppBarSearch';
 import LogoAndName from './elements/LogoAndName';
 import IconGroup from './elements/IconGroup';
+import clsx from 'clsx';
 
-export interface AppBarProps {}
+export interface AppBarProps {
+  app?: boolean;
+}
 
-const AppBar: React.FC<AppBarProps> = () => {
+const AppBar: React.FC<AppBarProps> = ({ app = false }) => {
   return (
-    <nav className="flex w-full justify-between px-4 dark:bg-midnight">
+    <nav
+      className={clsx(
+        {
+          'container mx-auto max-w-[1200px]': !app,
+          'w-full': app,
+        },
+        'flex justify-between rounded-full px-4 dark:bg-midnight',
+      )}
+    >
       <LogoAndName />
       <div className="flex items-center">
         <AppBarSearch />
