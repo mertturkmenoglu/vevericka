1975c30536a397d35176bcb68da4601792d14069
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-04-12 23:07:07 +0300

    Remove old API base url constant

--- service/constants.ts
+++ service/constants.ts
@@ -1,4 +1,3 @@
-// export const BASE_URL = 'https://vevericka-backend.herokuapp.com';
 export const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://api.vevericka.app' : 'http://localhost:5001';
 
 export const API_VER = 'v3';
