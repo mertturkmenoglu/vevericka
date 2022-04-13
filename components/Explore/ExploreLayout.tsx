fc5a1968d11fd5dbdbaf78f5a77c8ab3da38580f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-04-13 22:31:42 +0300

    Fix ExploreLayout dark theme title styles

--- components/Explore/ExploreLayout.tsx
+++ components/Explore/ExploreLayout.tsx
@@ -12,7 +12,7 @@ export interface ExploreLayoutProps {
 const ExploreLayout: React.FC<ExploreLayoutProps> = ({ title, isLoading, isError, children, className }) => {
   return (
     <article className={clsx('rounded-md', 'text-midnight', 'p-2', 'bg-white dark:bg-neutral-800', className)}>
-      <h2 className="border-b-2 border-midnight pb-2 text-xl font-bold dark:text-white">{title}</h2>
+      <h2 className="border-b-2 border-midnight pb-2 text-xl font-bold dark:border-white dark:text-white">{title}</h2>
 
       <LoadingLayout isLoading={isLoading} isError={isError}>
         {children}
