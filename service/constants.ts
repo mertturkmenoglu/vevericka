2b76387a7549041feb7322b984fad157614f7c5d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-04-14 21:51:35 +0300

    Update API constants

--- service/constants.ts
+++ service/constants.ts
@@ -1,3 +1,4 @@
-export const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://api.vevericka.app' : 'http://localhost:5001';
+export const BASE_URL =
+  process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_SERVICE_URL : 'http://localhost:5001';
 
 export const API_VER = 'v3';
