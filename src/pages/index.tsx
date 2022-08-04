c7f24b8383b368604a33d3183872dd2162f81a41
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-08-05 00:08:06 +0300

    Add content to landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,68 +1,71 @@
-import * as React from 'react';
-import { useEffect } from 'react';
+import React from 'react';
 import Head from 'next/head';
-
-import { useRouter } from 'next/router';
 import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/react';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { useTheme } from 'next-themes';
 import type { NextPage } from 'next';
+import useTranslation from 'next-translate/useTranslation';
+import { LandingAppBar } from '@components/index';
+import { Banner } from '@atom/index';
+import clsx from 'clsx';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const LandingPage: NextPage = () => {
-  const { setTheme } = useTheme();
   const { t } = useTranslation('landing');
-  const router = useRouter();
-
-  useEffect(() => {
-    const storage = new LocalStorage();
-    appContext.setIsDarkTheme(storage.isDarkTheme);
-    setTheme(storage.isDarkTheme ? 'dark' : 'light');
-    document.querySelector('body')?.classList?.add(BODY_CLASSNAME);
-  });
 
   return (
     <>
       <Head>
         <title>Vevericka</title>
-        <Seo
-          title="Vevericka"
-          description={t('title')}
-          og={{
-            siteName: 'Vevericka',
-            title: 'Vevericka',
-            description: t('title'),
-            image: 'https://imagedelivery.net/JVFt9XOk2_QXFDv2enrWOQ/c870d0f4-35a8-4ec9-c570-9c8082421700/public',
-            url: 'https://vevericka.app',
-            type: 'website',
-            locale: router.locale || 'en',
-          }}
-          twitter={{
-            card: 'summary',
-            title: 'Vevericka',
-            description: t('title'),
-            image: 'https://imagedelivery.net/JVFt9XOk2_QXFDv2enrWOQ/c870d0f4-35a8-4ec9-c570-9c8082421700/public',
-            imageAlt: t('meta.twitter.imageAlt'),
-            creator: '@capreaee',
-          }}
-        />
-        <meta
-          name="twitter:image"
-          content="https://imagedelivery.net/JVFt9XOk2_QXFDv2enrWOQ/c870d0f4-35a8-4ec9-c570-9c8082421700/public"
-        />
       </Head>
-      <div className="mx-auto">
-        <Banner appearance="primary">
-          <div className="flex items-center truncate text-ellipsis">
-            <span className="ml-2 text-white">{t('banner.text')}</span>
-          </div>
-        </Banner>
+
+      <Banner appearance="primary">
+        <div className="flex items-center truncate text-ellipsis">
+          <span className="ml-2 text-white">{t('banner.text')}</span>
+        </div>
+      </Banner>
+
+      <div className="mx-auto max-w-5xl">
+        <header>
+          <LandingAppBar className="mt-8" />
+        </header>
+
+        <main className="mx-auto mt-16 max-w-3xl text-center">
+          <h1
+            className={clsx(
+              'font-lato mx-8 mt-12 text-center text-3xl font-black leading-normal text-slate-700 dark:text-neutral-200 sm:mx-auto sm:mt-32 sm:w-2/3 sm:text-6xl'
+            )}
+          >
+            {t('title')}
+          </h1>
+
+          <p className="font-lato mx-8 mt-16 break-words text-center text-base leading-snug text-slate-700 dark:text-neutral-200 sm:mx-auto sm:mt-32 sm:text-2xl">
+            {t('content')}
+          </p>
+
+          <Link href="/login">
+            <a
+              className={clsx(
+                'mx-auto mt-16 w-64 text-center',
+                'flex items-center justify-center',
+                'transform transition-all duration-300 ease-in-out hover:scale-105',
+                'rounded-md bg-midnight px-8 py-2  dark:border dark:border-primary sm:px-16 sm:py-4'
+              )}
+            >
+              <Image
+                src="/assets/nut_white.svg"
+                width={32}
+                height={32}
+                alt="Nut"
+                className="pt-2"
+              />
+              <span className="font-lato ml-4 text-lg font-medium text-white">{t('cta')}</span>
+            </a>
+          </Link>
+        </main>
+
+        <footer></footer>
       </div>
-      <header className="mx-auto mt-8 w-11/12 rounded-full bg-white py-4 px-8 dark:bg-neutral-800 sm:w-2/3 md:w-1/2">
-        <LandingAppBar />
-      </header>
-      
-      <footer></footer>
     </>
   );
 };
