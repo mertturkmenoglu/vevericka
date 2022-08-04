d9cae2423b06087f7b64b398d42b1c6122cde0ad
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-08-04 21:28:44 +0300

    Update Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const colors = require('tailwindcss/colors');
+/** @type {import('tailwindcss').Config} */
 
 module.exports = {
   darkMode: 'class',
@@ -9,24 +9,24 @@ module.exports = {
     './layouts/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    colors: {
-      ...colors,
-      primary: '#be185d',
-      midnight: '#1a1a1a',
-      paper: {
-        50: '#f9f9f9',
-        100: '#f1f1f1',
-        200: '#acacac',
-        300: '#6c6c6c',
-        400: '#606060',
-        500: '#414141',
-        600: '#313131',
-        700: '#212121',
-        800: '#1a1a1a',
-        900: '#000000',
-      }
+    extend: {
+      colors: {
+        primary: '#be185d',
+        midnight: '#1a1a1a',
+        paper: {
+          50: '#f9f9f9',
+          100: '#f1f1f1',
+          200: '#acacac',
+          300: '#6c6c6c',
+          400: '#606060',
+          500: '#414141',
+          600: '#313131',
+          700: '#212121',
+          800: '#1a1a1a',
+          900: '#000000',
+        }
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
