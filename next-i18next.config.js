4d89ecf69139db44f899666d46adcff3fb081407
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-08-04 21:35:54 +0300

    Remove old config files

--- next-i18next.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es', 'tr'],
-    localePath: path.resolve('./public/static/locales'),
-  },
-};
