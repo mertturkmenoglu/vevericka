a0223a9f81ef917a13aef67ebba17938ec99628f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-08-05 02:25:15 +0300

    Update Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/layouts/**/*.{js,ts,jsx,tsx}',
+    './src/atom/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
