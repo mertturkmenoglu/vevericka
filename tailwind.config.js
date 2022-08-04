4a807a1754cab2832404bff0c50e0ad826f3a0f2
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-08-04 23:38:43 +0300

    Update Tailwind content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,9 @@
 module.exports = {
   darkMode: 'class',
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './atom/**/*.{js,ts,jsx,tsx}',
-    './layouts/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/layouts/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
