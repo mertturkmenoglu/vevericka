df8a9cc61b19753005b199ded3509f9984ac9cbf
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-08-05 12:04:12 +0300

    Add AppContext to application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,17 +6,20 @@ import { ReactQueryDevtools } from 'react-query/devtools';
 
 import '@styles/globals.css';
 import { ThemeProvider } from 'next-themes';
+import AppContextProvider from '@context/AppContext/AppContextProvider';
 
 function Application({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <QueryClientProvider client={new QueryClient()}>
-      <SessionProvider session={pageProps.session}>
-        <ThemeProvider attribute="class">
-          <Component {...pageProps} />
-          <ReactQueryDevtools />
-        </ThemeProvider>
-      </SessionProvider>
-    </QueryClientProvider>
+    <AppContextProvider>
+      <QueryClientProvider client={new QueryClient()}>
+        <SessionProvider session={pageProps.session}>
+          <ThemeProvider attribute="class">
+            <Component {...pageProps} />
+            <ReactQueryDevtools />
+          </ThemeProvider>
+        </SessionProvider>
+      </QueryClientProvider>
+    </AppContextProvider>
   );
 }
 
