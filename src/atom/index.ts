bf95c34ee5eee7281bed8b97b089b874a45898e1
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-08-05 04:45:53 +0300

    Add Avatar atomic component

--- src/atom/index.ts
+++ src/atom/index.ts
@@ -4,3 +4,4 @@ export { Banner } from './Banner';
 export { TextField } from './TextField';
 export { Button } from './Button';
 export { Spinner } from './Spinner';
+export { Avatar } from './Avatar';
