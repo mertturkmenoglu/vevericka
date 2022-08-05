6aeaca788e38e1075e491110abc2fa08cb970cbb
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-08-05 12:04:24 +0300

    Update feed page

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -1,29 +1,40 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Head from 'next/head';
 import { GetServerSideProps } from 'next';
-import { getSession, signOut } from 'next-auth/react';
+import { getSession } from 'next-auth/react';
 import type { NextPage } from 'next';
-import useTranslation from 'next-translate/useTranslation';
-import { useRouter } from 'next/router';
+import { AppBar } from '@components/AppBar';
+import { GetUserByUsernameResponse, UserApi } from '@services/user';
+import { AppContext } from '@context/AppContext';
 
-const FeedPage: NextPage = () => {
-  const { t } = useTranslation('landing');
-  const router = useRouter();
+export interface FeedPageProps {
+  user: GetUserByUsernameResponse;
+  userId: number;
+  token: string;
+}
+
+const FeedPage: NextPage<FeedPageProps> = ({ user }) => {
+  const ctx = useContext(AppContext);
+
+  useEffect(() => {
+    ctx.setUser((prev) => ({
+      ...prev,
+      email: user.email,
+      name: user.name,
+      image: user.image === 'profile.png' ? '/assets/profile.png' : user.image,
+      userId: user.id,
+      username: user.username,
+    }));
+  });
 
   return (
     <>
       <Head>
         <title>Vevericka</title>
       </Head>
-      Home {t('a')}
-      <button
-        onClick={async () => {
-          await signOut();
-          await router.push('/');
-        }}
-      >
-        logout
-      </button>
+      <header>
+        <AppBar className="mt-0 lg:mt-4" />
+      </header>
     </>
   );
 };
@@ -31,7 +42,7 @@ const FeedPage: NextPage = () => {
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context);
 
-  if (!session) {
+  if (!session || !session.user) {
     return {
       redirect: {
         destination: '/login',
@@ -40,8 +51,25 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     };
   }
 
+  const userApi = new UserApi(session.jwt);
+
+  const user = await userApi.getUserByUsername({ username: session.username });
+
+  if (!user) {
+    return {
+      redirect: {
+        destination: '/error',
+        permanent: false,
+      },
+    };
+  }
+
   return {
-    props: {},
+    props: {
+      user,
+      userId: session.id as number,
+      token: session.jwt,
+    },
   };
 };
 
