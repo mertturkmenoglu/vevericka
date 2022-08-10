fe8ef8c732bfe919a46bc6686f184204d5e6db60
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-08-11 02:29:36 +0300

    Update PostCard component

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -1,24 +1,14 @@
-import { Avatar } from '@atom/Avatar';
 import { FeedPost } from '@services/index';
 import clsx from 'clsx';
-import Link from 'next/link';
-import { BadgeCheckIcon } from '@heroicons/react/outline';
-import formatDistanceToNowStrict from 'date-fns/formatDistanceToNowStrict';
-import { useRouterDateLocale, useUserImage } from '@hooks/index';
-import { useMemo } from 'react';
+import Actions from './Actions';
+import Title from './Title';
+import Content from './Content';
 
 export interface PostCardProps {
   post: FeedPost;
 }
 
 function PostCard({ post }: PostCardProps): JSX.Element {
-  const image = useUserImage(post.user.image);
-  const dateLocal = useRouterDateLocale();
-
-  const formattedDate = useMemo(() => {
-    return formatDistanceToNowStrict(new Date(post.createdAt), { locale: dateLocal, addSuffix: true });
-  }, [post, dateLocal]);
-
   return (
     <article
       key={post.id}
@@ -31,32 +21,11 @@ function PostCard({ post }: PostCardProps): JSX.Element {
         'dark:pb-2'
       )}
     >
-      <div className="flex items-center">
-        <Link href={`/user/${post.user.username}`}>
-          <a>
-            <Avatar
-              src={image}
-              label={post.user.name}
-              appearance="circle"
-              size="large"
-            />
-          </a>
-        </Link>
-        <Link href={`/user/${post.user.username}`}>
-          <a className="ml-2">
-            <div className="flex items-center">
-              <p className="text-xl hover:underline">{post.user.name}</p>
-              {post.user.verified && <BadgeCheckIcon className="ml-1 mt-1 h-4 w-4 text-primary" />}
-            </div>
+      <Title post={post} />
 
-            <Link href={`/post/${post.id}`}>
-              <a className="text-xs text-slate-700 hover:underline dark:text-paper-200">{formattedDate}</a>
-            </Link>
-          </a>
-        </Link>
-      </div>
+      <Content post={post} />
 
-      <div className="mt-4 ml-2">{post.content}</div>
+      <Actions post={post} />
     </article>
   );
 }
