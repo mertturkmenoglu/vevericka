f51b751b107d81040793ce77610fbe28364cc134
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-08-11 02:28:13 +0300

    Update utility exports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,5 @@
 export * from './isElementInViewport';
 export * from './getDateLocaleFromRouterLocale';
+export * from './getNumberFormatter';
+export * from './getYouTubeIframe';
+export * from './preparePostText';
