73a7efbf3e1c23bc80dfe58d4fdbd43dae853a86
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-08-27 00:07:50 +0300

    Update hooks folder barrel file

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useLanguages } from './useLanguages';
 export { useUserImage } from './useUserImage';
 export { useRouterDateLocale } from './useRouterDateLocale';
+export * from './useAppUser';
