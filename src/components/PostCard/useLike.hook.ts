174ed76bae60cf995e8e03f08c956efb2baad56d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-08-26 23:55:15 +0300

    Update useLike hook

--- src/components/PostCard/useLike.hook.ts
+++ src/components/PostCard/useLike.hook.ts
@@ -1,50 +1,31 @@
-import { FeedPost, LikeStatus, PostApi } from '@services/index';
-import { useSession } from 'next-auth/react';
-import { useState, useEffect } from 'react';
+import { FeedPost, LikeStatus, PostApi } from '@services/post';
+import { useRef } from 'react';
 import { useQueryClient } from 'react-query';
 
 export function useLike(post: FeedPost) {
-  const { data: session, status: authStatus } = useSession();
-  const [postApi, setPostApi] = useState<PostApi | null>(null);
-
+  const postApi = useRef(new PostApi()).current;
   const queryClient = useQueryClient();
 
-  useEffect(() => {
-    if (authStatus !== 'authenticated' || !session) {
-      return;
-    }
-
-    setPostApi(new PostApi(session.jwt));
-  }, [session, authStatus, setPostApi]);
-
   const like = async () => {
-    if (!postApi) {
-      return;
-    }
-
     if (post.likeStatus === LikeStatus.LIKED) {
       await postApi.likeNone(post.id);
     } else {
       await postApi.likePost(post.id);
     }
 
-    queryClient.invalidateQueries('feed');
-    queryClient.refetchQueries('feed');
+    await queryClient.invalidateQueries('feed');
+    await queryClient.refetchQueries('feed');
   };
 
   const dislike = async () => {
-    if (!postApi) {
-      return;
-    }
-
     if (post.likeStatus === LikeStatus.DISLIKED) {
       await postApi.likeNone(post.id);
     } else {
       await postApi.dislikePost(post.id);
     }
 
-    queryClient.invalidateQueries('feed');
-    queryClient.refetchQueries('feed');
+    await queryClient.invalidateQueries('feed');
+    await queryClient.refetchQueries('feed');
   };
 
   return {
