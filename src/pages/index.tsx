6fadf4ef9bbe6ee4503ed6956398c2a66d4be572
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-08-30 13:59:46 +0300

    Add blog posts carousel to landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Head from 'next/head';
+import type { NextPage } from 'next';
 import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/react';
-import type { NextPage } from 'next';
 import useTranslation from 'next-translate/useTranslation';
 import { LandingAppBar } from '@components/index';
 import { Banner } from '@atom/index';
 import clsx from 'clsx';
 import Link from 'next/link';
 import Image from 'next/image';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/a11y';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { A11y, Navigation } from 'swiper';
+import { useQuery } from 'react-query';
+import { BlogApi } from '@services/blog';
+import LoadingLayout from '@layouts/LoadingLayout';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
 
 const LandingPage: NextPage = () => {
   const { t } = useTranslation('landing');
+  const blogApi = useRef(new BlogApi());
+  const {
+    data: playlists,
+    isLoading,
+    isError,
+  } = useQuery(['landingPlaylists'], async () => {
+    return blogApi.current.getAllPlaylists();
+  });
 
   return (
     <>
@@ -30,16 +50,16 @@ const LandingPage: NextPage = () => {
           <LandingAppBar className="mt-8" />
         </header>
 
-        <main className="mx-auto mt-16 max-w-3xl text-center">
+        <main className="mx-auto mt-16 max-w-5xl text-center">
           <h1
             className={clsx(
-              'font-lato mx-8 mt-12 text-center text-3xl font-black leading-normal text-slate-700 dark:text-neutral-200 sm:mx-auto sm:mt-32 sm:w-2/3 sm:text-6xl'
+              'font-lato mx-8 mt-12 text-center text-3xl font-black leading-normal text-slate-700 dark:text-neutral-200 sm:mx-auto sm:mt-32 sm:w-2/3 sm:text-4xl'
             )}
           >
             {t('title')}
           </h1>
 
-          <p className="font-lato mx-8 mt-16 break-words text-center text-base leading-snug text-slate-700 dark:text-neutral-200 sm:mx-auto sm:mt-32 sm:text-2xl">
+          <p className="font-lato mx-8 mt-16 max-w-3xl break-words text-center text-base leading-snug text-slate-700 dark:text-neutral-200 sm:mx-auto sm:text-xl">
             {t('content')}
           </p>
 
@@ -62,6 +82,58 @@ const LandingPage: NextPage = () => {
               <span className="font-lato ml-4 text-lg font-medium text-white">{t('cta')}</span>
             </a>
           </Link>
+
+          <div className="font-lato mt-16 w-full text-center text-3xl font-black text-slate-700">
+            <h2>Latest posts from our blog</h2>
+            <LoadingLayout
+              isLoading={isLoading}
+              isError={isError}
+            >
+              <div className="flex items-center">
+                <button className="swiper-button-prev-unique">
+                  <ChevronLeftIcon className="h-6 w-6 text-midnight" />
+                  <span className="sr-only">Previous</span>
+                </button>
+                <Swiper
+                  className="mt-8 flex items-center"
+                  slidesPerView={3}
+                  spaceBetween={80}
+                  modules={[A11y, Navigation]}
+                  loopFillGroupWithBlank={false}
+                  loop={true}
+                  navigation={{
+                    enabled: true,
+                    prevEl: '.swiper-button-prev-unique',
+                    nextEl: '.swiper-button-next-unique',
+                  }}
+                >
+                  {(playlists ?? []).map((playlist) => (
+                    <SwiperSlide
+                      key={playlist.title}
+                      className="flex items-center"
+                    >
+                      <a
+                        href={`/blog/playlist/${playlist.startsAt.split('-')[0]}/${playlist.startsAt.split('-')[1]}`}
+                        className="flex min-h-[288px] flex-col items-center rounded-xl bg-paper-50 p-4 text-center"
+                      >
+                        <Image
+                          src={'http:' + playlist.thumbnail.fields.file.url}
+                          width={192}
+                          height={192}
+                          alt={playlist.thumbnail.fields.description}
+                        />
+                        <p className="mt-4 text-sm">{playlist.title}</p>
+                      </a>
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+                <button className="swiper-button-next-unique">
+                  <ChevronRightIcon className="h-6 w-6 text-midnight" />
+                  <span className="sr-only">Next</span>
+                </button>
+              </div>
+            </LoadingLayout>
+          </div>
         </main>
 
         <footer></footer>
