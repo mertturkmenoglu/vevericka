fbaf048d2bbc0b6b1da7767bd1bce11fcfa9cc74
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-12-26 00:13:06 +0300

    Reformat files

--- apps/web/src/components/Button/Button.tsx
+++ apps/web/src/components/Button/Button.tsx
@@ -1,18 +1,18 @@
-import React from "react";
+import React from 'react';
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ text, ...props }, ref) => {
-    return (
-      <button ref={ref} {...props}>
-        {text}
-      </button>
-    );
-  }
-);
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ text, ...props }, ref) => {
+  return (
+    <button
+      ref={ref}
+      {...props}
+    >
+      {text}
+    </button>
+  );
+});
 
 export default Button;
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,10 +2,11 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
   test: {
     globals: true,
     environment: "jsdom",
