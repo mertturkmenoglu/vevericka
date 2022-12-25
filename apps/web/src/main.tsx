0f39241d6257081a0b1f1950e5e1b20e47810e8b
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-12-26 00:40:35 +0300

    Add Tanstack Query

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { createReactRouter, createRouteConfig, Outlet, RouterProvider } from '@tanstack/react-router';
 import './index.css';
 import { FeedPage, HomePage } from './pages';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const TanStackRouterDevtools = import.meta.env.PROD
   ? () => null // Render nothing in production
@@ -35,12 +37,14 @@ const routeConfig = rootRoute.addChildren([homeRoute, feedRoute]);
 
 const router = createReactRouter({ routeConfig });
 
+const queryClient = new QueryClient();
+const TANSTACK_DEVTOOLS: 'router' | 'query' = 'query';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    <TanStackRouterDevtools
-      router={router}
-      initialIsOpen={false}
-    />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      {TANSTACK_DEVTOOLS === 'query' ? <ReactQueryDevtools /> : <TanStackRouterDevtools router={router} />}
+    </QueryClientProvider>
   </React.StrictMode>
 );
