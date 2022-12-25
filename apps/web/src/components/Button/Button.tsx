13e99e4181fb5e81d64978c18993fcba3bdec697
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-12-25 23:38:43 +0300

    Add example button component

---
+++ apps/web/src/components/Button/Button.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  text: string;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ text, ...props }, ref) => {
+    return (
+      <button ref={ref} {...props}>
+        {text}
+      </button>
+    );
+  }
+);
+
+export default Button;
