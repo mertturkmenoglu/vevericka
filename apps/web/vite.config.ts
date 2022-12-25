c2930b5c04c69dd732823f23ea528a36c1985179
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-12-25 23:59:07 +0300

    Add Vitest to project

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,17 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    css: true,
+  },
 });
