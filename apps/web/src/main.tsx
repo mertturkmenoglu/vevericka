aa5463ee9afb175f25e2bb2ad591c6c2f1a99d82
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-12-27 18:09:17 +0300

    feat(web): update routing

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,57 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createReactRouter, createRouteConfig, Outlet, RouterProvider } from '@tanstack/react-router';
 import './index.css';
-import { FeedPage, HomePage, LoginPage } from './pages';
+import { FeedPage, HomePage, LoginPage, NotFoundPage } from './pages';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import './i18n';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const TanStackRouterDevtools = import.meta.env.PROD
-  ? () => null // Render nothing in production
-  : React.lazy(() =>
-      // Lazy load in development
-      import('@tanstack/react-router-devtools').then((res) => ({
-        default: res.TanStackRouterDevtools,
-      }))
-    );
-
-const rootRoute = createRouteConfig({
-  component: () => (
-    <>
-      <Outlet />
-    </>
-  ),
-});
-
-const feedRoute = rootRoute.createRoute({
-  path: '/feed',
-  component: FeedPage,
-});
-
-const homeRoute = rootRoute.createRoute({
-  path: '/',
-  component: HomePage,
-});
-
-const loginRoute = rootRoute.createRoute({
-  path: '/login',
-  component: LoginPage,
-});
-
-const routeConfig = rootRoute.addChildren([feedRoute, homeRoute, loginRoute]);
-
-const router = createReactRouter({ routeConfig });
+const router = createBrowserRouter([
+  {
+    path: '/feed',
+    element: <FeedPage />,
+  },
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+]);
 
 const queryClient = new QueryClient();
-const TANSTACK_DEVTOOLS: 'router' | 'query' = 'query';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      {TANSTACK_DEVTOOLS === 'query' ? <ReactQueryDevtools /> : <TanStackRouterDevtools router={router} />}
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
