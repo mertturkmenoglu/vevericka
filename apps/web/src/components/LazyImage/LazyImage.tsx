69e9b3f0d9d1f507cc05d1c54f10b1f554ccaecb
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-12-27 21:30:36 +0300

    feat(web): add lazy image component

---
+++ apps/web/src/components/LazyImage/LazyImage.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+export interface LazyImageProps {
+  src: string;
+  placeholderSrc: string;
+  alt: string;
+  placeholderAlt: string;
+  className?: string;
+}
+
+function LazyImage({ src, placeholderSrc, alt, placeholderAlt, className }: LazyImageProps): JSX.Element {
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <>
+      <img
+        src={placeholderSrc}
+        alt={placeholderAlt}
+        style={{ display: isLoading ? 'block' : 'none' }}
+        className={className}
+      />
+      <img
+        src={src}
+        alt={alt}
+        style={{ display: isLoading ? 'none' : 'block' }}
+        onLoad={() => setIsLoading(false)}
+        className={className}
+      />
+    </>
+  );
+}
+
+export default LazyImage;
