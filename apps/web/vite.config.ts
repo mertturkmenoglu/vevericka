bb4c3939811cdb943b4533449e1b4dac728d6cf0
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2022-12-27 20:28:32 +0300

    feat(web): add svgr plugin to vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,16 +1,18 @@
 /// <reference types="vitest" />
+/// <reference types="vite-plugin-svgr/client" />
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import eslint from 'vite-plugin-eslint'
+import eslint from "vite-plugin-eslint";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react(), eslint(), svgr()],
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
     css: true,
