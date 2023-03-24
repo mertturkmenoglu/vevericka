6a98e9decf963594be0aad17442870afe4c77dde
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-03-24 13:44:31 +0300

    Add auth guards

---
+++ apps/api/src/auth/guards/discord.guard.ts
@@ -0,0 +1,5 @@
+import { Injectable } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+
+@Injectable()
+export class DiscordGuard extends AuthGuard("discord") {}
---
+++ apps/api/src/auth/guards/index.ts
@@ -0,0 +1,6 @@
+export * from "./github.guard";
+export * from "./spotify.guard";
+export * from "./twitter.guard";
+export * from "./jwt.guard";
+export * from "./google.guard";
+export * from "./discord.guard";
