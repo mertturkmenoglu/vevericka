a38d6668bc511621bae9e888ba91a1f08e81f218
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-03-27 16:33:11 +0300

    Move useAuth hook

--- apps/web/src/components/AuthGuards/GuestRoute.tsx
+++ apps/web/src/components/AuthGuards/GuestRoute.tsx
@@ -1,5 +1,5 @@
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../../hooks/useAuth';
+import { useAuth } from '../../hooks';
 
 export interface GuestRouteProps {
   children: React.ReactNode;
