14d8bff36b20d5706f232a341ce27acb5315776a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-03-27 13:43:54 +0300

    Add cookie and redirect logic to OAuth callback endpoints

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,128 +1,100 @@
-import { Controller, Get, Req, Res, UseGuards } from "@nestjs/common";
-import { Response } from "express";
+import { Controller, Get, Post, Req, Res, UseGuards } from "@nestjs/common";
+import { Profile } from "passport";
 import {
   SpotifyGuard,
   GitHubGuard,
   TwitterGuard,
   GoogleGuard,
   DiscordGuard,
+  JwtAuthGuard,
 } from "./guards";
 import { AuthService } from "./auth.service";
-import { JwtService } from "@nestjs/jwt";
+import { Request, Response } from "express";
+import { ConfigService } from "@nestjs/config";
 
 @Controller("oauth")
 export class AuthController {
   constructor(
     private readonly authService: AuthService,
-    private readonly jwtService: JwtService
+    private readonly configService: ConfigService
   ) {}
+
+  @Post("logout")
+  @UseGuards(JwtAuthGuard)
+  logout(@Res() res: Response) {
+    // TODO: Fix cookie remove bug
+    return res
+      .cookie("jwt-access", "", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",
+        maxAge: 1,
+        path: "/",
+      })
+      .redirect(this.configService.get<string>("WEB_CLIENT_URL"));
+  }
 
   @Get("google")
   @UseGuards(GoogleGuard)
-  async googleLogin() {
-    console.log("google login called");
-    return "google ok";
-  }
+  async googleLogin() {}
 
   @Get("github")
   @UseGuards(GitHubGuard)
-  async githubLogin() {
-    console.log("github login called");
-    return "github ok";
-  }
+  async githubLogin() {}
 
   @Get("twitter")
   @UseGuards(TwitterGuard)
-  async twitterLogin() {
-    console.log("twitter login called");
-    return "twitter ok";
-  }
+  async twitterLogin() {}
 
   @Get("spotify")
   @UseGuards(SpotifyGuard)
-  async spotifyLogin() {
-    console.log("spotify login called");
-    return "spotify ok";
-  }
+  async spotifyLogin() {}
 
   @Get("discord")
   @UseGuards(DiscordGuard)
-  async discordLogin() {
-    console.log("discord login called");
-    return "discord ok";
-  }
+  async discordLogin() {}
 
   @Get("callback/google")
   @UseGuards(GoogleGuard)
-  googleAuthCallback(@Req() req: any) {
-    const user = req.user;
-    console.log(user);
-    const payload = { sub: user.id, username: user.username };
-    return { accessToken: this.jwtService.sign(payload) };
+  googleAuthCallback(@Req() req: Request, @Res() res: Response) {
+    return this.addCookieAndRedirect(req, res);
   }
 
   @Get("callback/github")
   @UseGuards(GitHubGuard)
-  githubAuthCallback(@Req() req: any) {
-    const user = req.user;
-    console.log(user);
-    const payload = { sub: user.id, username: user.username };
-    return { accessToken: this.jwtService.sign(payload) };
+  githubAuthCallback(@Req() req: Request, @Res() res: Response) {
+    return this.addCookieAndRedirect(req, res);
   }
 
   @Get("callback/twitter")
   @UseGuards(TwitterGuard)
-  twitterAuthCallback(@Req() req: any) {
-    const user = req.user;
-    const payload = { sub: user.id, username: user.username };
-    return { accessToken: this.jwtService.sign(payload) };
+  twitterAuthCallback(@Req() req: Request, @Res() res: Response) {
+    return this.addCookieAndRedirect(req, res);
   }
 
   @Get("callback/spotify")
   @UseGuards(SpotifyGuard)
-  spotifyAuthCallback(@Req() req: any) {
-    const user = req.user;
-    const payload = { sub: user.id, username: user.username };
-    return { accessToken: this.jwtService.sign(payload) };
+  spotifyAuthCallback(@Req() req: Request, @Res() res: Response) {
+    return this.addCookieAndRedirect(req, res);
   }
 
   @Get("callback/discord")
   @UseGuards(DiscordGuard)
-  discordAuthCallback(@Req() req: any) {
-    const user = req.user;
-    const payload = { sub: user.id, username: user.username };
-    return { accessToken: this.jwtService.sign(payload) };
+  discordAuthCallback(@Req() req: Request, @Res() res: Response) {
+    return this.addCookieAndRedirect(req, res);
   }
 
-  // @UseGuards(SpotifyOauthGuard)
-  // @Get("redirect")
-  // async spotifyAuthRedirect(
-  //   @Req() req: any,
-  //   @Res() res: Response
-  // ): Promise<Response> {
-  //   const {
-  //     user,
-  //     authInfo,
-  //   }: {
-  //     user: Profile;
-  //     authInfo: {
-  //       accessToken: string;
-  //       refreshToken: string;
-  //       expires_in: number;
-  //     };
-  //   } = req;
-
-  //   if (!user) {
-  //     res.redirect("/");
-  //     return;
-  //   }
-
-  //   req.user = undefined;
-
-  //   const jwt = this.authService.login(user);
-
-  //   res.set("authorization", `Bearer ${jwt}`);
-
-  //   return res.status(201).json({ authInfo, user });
-  // }
+  private addCookieAndRedirect(@Req() req: Request, @Res() res: Response) {
+    const BASE = this.configService.get<string>("WEB_CLIENT_URL");
+    const webClientRedirectUrl = `${BASE}/feed`;
+    const user: Profile = req.user as Profile;
+    const token = this.authService.login(user);
+    return res
+      .cookie("jwt-access", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        path: "/",
+      })
+      .redirect(webClientRedirectUrl);
+  }
 }
