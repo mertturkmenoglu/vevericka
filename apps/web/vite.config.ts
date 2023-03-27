97a313d7914161dbc485673bffe73d1593bc9aa2
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-03-27 17:46:34 +0300

    Add Vite codegen plugin

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,14 +1,15 @@
 /// <reference types="vitest" />
 /// <reference types="vite-plugin-svgr/client" />
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import eslint from "vite-plugin-eslint";
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import eslint from 'vite-plugin-eslint';
+import svgr from 'vite-plugin-svgr';
+import codegen from 'vite-plugin-graphql-codegen';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint(), svgr()],
+  plugins: [react(), eslint({ exclude: ['./src/generated'] }), svgr(), codegen()],
   test: {
     globals: true,
     environment: 'jsdom',
