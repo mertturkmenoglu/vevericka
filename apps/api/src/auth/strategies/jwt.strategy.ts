f0f16b17cf0ce9eab4ad9448898982c3282691ae
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-03-27 13:42:48 +0300

    Update JWT extract method

--- apps/api/src/auth/strategies/jwt.strategy.ts
+++ apps/api/src/auth/strategies/jwt.strategy.ts
@@ -1,17 +1,25 @@
+import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { AuthService } from "../auth.service";
+import { JwtPayload } from "../types/jwt-payload.type";
+import { Request } from "express";
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
   constructor(private readonly authService: AuthService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          return request?.cookies?.["jwt-access"];
+        },
+      ]),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET,
     });
   }
 
-  async validate(payload: { username: string; sub: string }) {
+  async validate(payload: JwtPayload) {
     return this.authService.findUserById(payload.sub);
   }
 }
