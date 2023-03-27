a470a3daf07da83f992fddfabc743f51a12492fe
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-03-28 02:45:31 +0300

    Update feed query parameter type on web app

--- apps/web/src/graphql/queries/feedQuery.ts
+++ apps/web/src/graphql/queries/feedQuery.ts
@@ -1,7 +1,7 @@
 import { graphql } from '../../generated';
 
 export const feedQueryDocument = graphql(/* GraphQL */ `
-  query feedQuery($id: Float!, $skip: Int, $take: Int) {
+  query feedQuery($id: String!, $skip: Int, $take: Int) {
     feed(id: $id, skip: $skip, take: $take) {
       posts {
         id
