1dc83771383e2684ddde833fae6dd54e4a3deec8
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-03-28 04:47:58 +0300

    Add post utilities

---
+++ apps/web/src/lib/post/content.tsx
@@ -0,0 +1,6 @@
+import { detectHashtags, detectUsernames, linkify } from './post-utils';
+
+export const getPostContent = (text: string): JSX.Element => {
+  const content = linkify(detectUsernames(detectHashtags(text)));
+  return <div dangerouslySetInnerHTML={{ __html: content }} />;
+};
---
+++ apps/web/src/lib/post/post-utils.ts
@@ -0,0 +1,29 @@
+const URL_REGEX = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gi;
+const USERNAME_REGEX = /@[-A-Z0-9_]+/gi;
+const HASHTAG_REGEX = /#[-A-Z0-9_]+/gi;
+
+const className = 'text-berry hover:underline';
+
+export const detectHashtags = (text: string): string =>
+  text.replace(HASHTAG_REGEX, (h) => `<a href='/explore/${h.substr(1)}' class='${className}'>${h}</a>`);
+
+export const detectUsernames = (text: string): string =>
+  text.replace(USERNAME_REGEX, (u) => `<a href='/user/${u.substr(1)}' class='${className}'>${u}</a>`);
+
+export const linkify = (text: string): string =>
+  text.replace(URL_REGEX, (url) => `<a href='${url}' class='${className}'>${url}</a>`);
+
+export const preparePostText = (text: string): string => {
+  const usernamesDetected = detectUsernames(text);
+  const hashtagsDetected = detectHashtags(usernamesDetected);
+  return linkify(hashtagsDetected);
+};
+
+export const copyToClipboard = (text: string): void => {
+  const tmpInput = document.createElement('input');
+  tmpInput.value = text;
+  document.body.appendChild(tmpInput);
+  tmpInput.select();
+  document.execCommand('copy');
+  document.body.removeChild(tmpInput);
+};
