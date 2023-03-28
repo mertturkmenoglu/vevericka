2e73b81449a678ccc80a82f7ac9d65b3873559cf
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-03-28 03:43:54 +0300

    Add auth guard and user check to feed resolver

--- apps/api/src/feed/feed.resolver.ts
+++ apps/api/src/feed/feed.resolver.ts
@@ -1,5 +1,8 @@
-import { NotFoundException } from "@nestjs/common";
+import { NotFoundException, UseGuards } from "@nestjs/common";
 import { Args, Query, Resolver } from "@nestjs/graphql";
+import { JwtAuthGuard } from "src/auth/guards";
+import { CurrentUser as CurrentUserDecorator } from "src/common/decorators/current-user.decorator";
+import { CurrentUser } from "src/common/types/current-user.type";
 import { PaginationArgs } from "../common/args/pagination.args";
 import { FeedService } from "./feed.service";
 import { Feed } from "./models/feed.model";
@@ -9,13 +12,17 @@ export class FeedResolver {
   constructor(private readonly feedService: FeedService) {}
 
   @Query(() => Feed)
+  @UseGuards(JwtAuthGuard)
   async feed(
-    @Args("id") id: string,
+    @CurrentUserDecorator() currentUser: CurrentUser,
     @Args() pagination: PaginationArgs
   ): Promise<Feed> {
-    const feed = await this.feedService.getUserFeed(id, pagination);
+    const feed = await this.feedService.getUserFeed(
+      currentUser.user.id,
+      pagination
+    );
     if (!feed) {
-      throw new NotFoundException(id);
+      throw new NotFoundException(currentUser.user.id);
     }
     return feed;
   }
