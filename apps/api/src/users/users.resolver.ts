e05bab3a013ef0b0ebdb4177ccd7bcb2f12bb319
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-03-28 11:09:58 +0300

    Cleanup users resolver

--- apps/api/src/users/users.resolver.ts
+++ apps/api/src/users/users.resolver.ts
@@ -6,29 +6,24 @@ import { CurrentUser } from "src/common/types/current-user.type";
 import { User } from "./models/user.model";
 import { UsersService } from "./users.service";
 
-@Resolver((of) => User)
+@Resolver(() => User)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Query(() => User)
   @UseGuards(JwtAuthGuard)
   async me(@CurrentUserDecorator() currentUser: CurrentUser): Promise<User> {
-    const user = await this.usersService.findOneById(`${currentUser.user.id}`);
+    const user = await this.usersService.findOneById(currentUser.user.id);
     return user;
   }
 
   @Query(() => User)
   @UseGuards(JwtAuthGuard)
   async user(@Args("id") id: string): Promise<User> {
-    const post = await this.usersService.findOneById(id);
-    if (!post) {
+    const user = await this.usersService.findOneById(id);
+    if (!user) {
       throw new NotFoundException(id);
     }
-    return post;
-  }
-
-  @Query(() => [User])
-  users(): Promise<User[]> {
-    return this.usersService.findAll();
+    return user;
   }
 }
