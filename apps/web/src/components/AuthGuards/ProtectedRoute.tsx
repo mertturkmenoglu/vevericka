848810fa7ec1a970961c90fc0ba13a6ea5652f02
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-04-07 00:13:23 +0300

    Add updateLastSeen mutation call to protected routes

--- apps/web/src/components/AuthGuards/ProtectedRoute.tsx
+++ apps/web/src/components/AuthGuards/ProtectedRoute.tsx
@@ -1,5 +1,8 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../../hooks';
+import { useEffect } from 'react';
+import { useMutation } from '@apollo/client';
+import { updateLastSeenDocument } from '../../graphql';
 
 export interface ProtectedRouteProps {
   children: React.ReactNode;
@@ -7,9 +10,24 @@ export interface ProtectedRouteProps {
 
 function ProtectedRoute({ children }: ProtectedRouteProps) {
   const { isAuthenticated, loading } = useAuth();
+  const [updateLastSeen] = useMutation(updateLastSeenDocument);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (isAuthenticated && !loading) {
+        updateLastSeen();
+      }
+    }, 1000 * 60 * 2);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isAuthenticated, loading]);
+
   if (!loading && !isAuthenticated) {
     return <Navigate to="/" />;
   }
+
   return <>{children}</>;
 }
 
