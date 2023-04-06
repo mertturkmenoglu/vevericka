dfbbc64606892e9db39362d7d33b5a0c7898a868
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-04-07 00:12:20 +0300

    Add lastSeen mutation and subscription

--- apps/api/src/users/users.resolver.ts
+++ apps/api/src/users/users.resolver.ts
@@ -1,10 +1,14 @@
 import { NotFoundException, UseGuards } from "@nestjs/common";
-import { Args, Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver, Subscription } from "@nestjs/graphql";
 import { JwtAuthGuard } from "src/auth/guards";
 import { CurrentUser as CurrentUserDecorator } from "../common/decorators/current-user.decorator";
 import { CurrentUser } from "src/common/types/current-user.type";
 import { User } from "./models/user.model";
 import { UsersService } from "./users.service";
+import { PubSub } from "graphql-subscriptions";
+import { LastSeen } from "./models/last-seen.model";
+
+const pubSub = new PubSub();
 
 @Resolver(() => User)
 export class UsersResolver {
@@ -25,5 +29,22 @@ export class UsersResolver {
       throw new NotFoundException(id);
     }
     return user;
+  }
+
+  @Subscription(() => LastSeen)
+  lastSeen() {
+    return pubSub.asyncIterator("lastSeen");
+  }
+
+  @Mutation(() => String)
+  @UseGuards(JwtAuthGuard)
+  async updateLastSeen(@CurrentUserDecorator() currentUser: CurrentUser) {
+    await pubSub.publish("lastSeen", {
+      lastSeen: {
+        id: currentUser.user.id,
+        date: new Date().toISOString(),
+      },
+    });
+    return "ok";
   }
 }
