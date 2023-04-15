c94757a10b7869f68c6942a3d52d2b21294a640a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-04-15 23:25:48 +0300

    Update ExploreCard styles and add number formatting

--- apps/web/src/components/ExploreCard/ExploreCard.tsx
+++ apps/web/src/components/ExploreCard/ExploreCard.tsx
@@ -7,23 +7,43 @@ import { Link } from 'react-router-dom';
 const explore = [
   {
     tag: 'Vevericka',
-    count: 100000,
+    count: 100_000_000,
   },
   {
     tag: 'Jean Valjean',
-    count: 24601,
+    count: 10_000_000,
+  },
+  {
+    tag: 'Marius',
+    count: 1_000_000,
+  },
+  {
+    tag: 'Cosette',
+    count: 987_654,
+  },
+  {
+    tag: 'Fantine',
+    count: 78_192,
+  },
+  {
+    tag: 'Monsieur Madeleine',
+    count: 50_121,
+  },
+  {
+    tag: 'Gavroche',
+    count: 9230,
   },
   {
     tag: 'Javert',
-    count: 9876,
+    count: 1002,
   },
   {
     tag: 'Enjolras',
-    count: 987,
+    count: 947,
   },
   {
     tag: 'Eponine',
-    count: 87,
+    count: 32,
   },
 ];
 
@@ -32,42 +52,44 @@ interface Props {
 }
 
 function ExploreCard({ className }: Props): JSX.Element {
-  return (
-    <article className={clsx('rounded bg-neutral-100 py-4 px-6', className)}>
-      <h2 className="text-xl font-normal text-midnight">Explore Vevericka</h2>
+  const formatCount = (count: number) => {
+    return Intl.NumberFormat('en-US', { notation: 'compact' }).format(count);
+  };
 
-      <hr className="mt-1 rounded border border-midnight/10" />
+  return (
+    <article className={clsx('rounded bg-neutral-100 py-4', className)}>
+      <h2 className="px-6 text-xl font-normal text-midnight">Explore Vevericka</h2>
+
+      <hr className="mx-6 mt-1 rounded border border-midnight/10" />
 
       <div className="mt-2 space-y-2">
         {explore.map((it) => (
           <Link
             key={it.tag}
             to={'#'}
-            className="flex items-center justify-between rounded px-2 py-1 text-midnight transition-all duration-200 hover:bg-midnight/10"
+            className="flex items-center justify-between py-1 text-midnight transition-all duration-200 hover:bg-midnight/10"
           >
-            <div>
-              <div>
-                <span className="text-lg font-light text-berry">#</span>
-                <span className="ml-2 text-lg">{it.tag}</span>
-              </div>
-              <div className="ml-[18px] flex items-baseline text-sm">
-                <span className="font-normal">{it.count}</span>
+            <div className="px-6">
+              <span className="font-medium">{it.tag}</span>
+
+              <div className="flex items-baseline text-sm">
+                <span className="font-normal">{formatCount(it.count)}</span>
                 <span className="ml-1 font-light">posts</span>
               </div>
             </div>
 
-            <div>
+            <div className="px-6">
               <ChevronRightIcon className="h-4 w-4" />
             </div>
           </Link>
         ))}
       </div>
 
-      <hr className="mt-2 rounded border border-midnight/10" />
+      <hr className="mx-6 mt-2 rounded border border-midnight/10" />
 
       <Link
         to={'#'}
-        className="mt-2 flex justify-end  text-sm font-semibold text-midnight "
+        className="mt-2 flex justify-end  px-6 text-sm font-semibold text-midnight"
       >
         <span className="items-end self-end rounded px-2 py-1 hover:bg-midnight/10">More</span>
       </Link>
