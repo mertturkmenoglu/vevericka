a55f4a022234cd9a56ea8a3e3aef9d6c659bda04
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-04-15 22:59:48 +0300

    Change post content styles

--- apps/web/src/lib/post/content.tsx
+++ apps/web/src/lib/post/content.tsx
@@ -2,5 +2,10 @@ import { detectHashtags, detectUsernames, linkify } from './post-utils';
 
 export const getPostContent = (text: string): JSX.Element => {
   const content = linkify(detectUsernames(detectHashtags(text)));
-  return <div dangerouslySetInnerHTML={{ __html: content }} />;
+  return (
+    <div
+      dangerouslySetInnerHTML={{ __html: content }}
+      className="text-sm"
+    />
+  );
 };
