983f08f02a567c7d060081ab2990536fd7a39edf
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-04-16 19:37:00 +0300

    Update user detail page

--- apps/web/src/pages/user/User.tsx
+++ apps/web/src/pages/user/User.tsx
@@ -1,15 +1,19 @@
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 import { MainLayout } from '../../layouts';
 import { GetProfileDataQuery } from '../../generated/graphql';
 import { LazyImage, PostCard } from '../../components';
 import { CheckBadgeIcon, PlusIcon } from '@heroicons/react/24/outline';
 import { useFragment } from '../../generated';
 import { postFragmentDocument, profileFragmentDocument } from '../../graphql';
+import { useAppStore } from '../../stores';
 
 function User(): JSX.Element {
+  const appUser = useAppStore((state) => state.user);
   const data = useLoaderData() as GetProfileDataQuery;
   const user = useFragment(profileFragmentDocument, data.profile);
   const posts = useFragment(postFragmentDocument, data.posts);
+
+  const isThisUser = appUser?.id === user.id;
 
   return (
     <MainLayout>
@@ -47,13 +51,26 @@ function User(): JSX.Element {
           </div>
 
           <div className="flex items-center space-x-4">
-            <button className="flex items-center rounded bg-midnight px-4 py-2 text-white transition ease-in-out hover:bg-midnight/90">
-              <PlusIcon className="h-5 w-5 text-white" />
-              <span className="ml-2">Follow</span>
-            </button>
-            <button className="rounded bg-neutral-200 px-4 py-2 text-neutral-600 transition ease-in-out hover:bg-neutral-200/60">
-              Message
-            </button>
+            {!isThisUser && (
+              <>
+                <button className="flex items-center rounded bg-midnight px-4 py-2 text-white transition ease-in-out hover:bg-midnight/90">
+                  <PlusIcon className="h-5 w-5 text-white" />
+                  <span className="ml-2">Follow</span>
+                </button>
+
+                <button className="rounded bg-neutral-200 px-4 py-2 text-neutral-600 transition ease-in-out hover:bg-neutral-200/60">
+                  Message
+                </button>
+              </>
+            )}
+            {isThisUser && (
+              <Link
+                to={'/settings'}
+                className="rounded bg-neutral-200 px-4 py-2 text-neutral-600 transition ease-in-out hover:bg-neutral-200/60"
+              >
+                Settings
+              </Link>
+            )}
           </div>
         </div>
 
