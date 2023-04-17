86915d12cf0fcacccfb84512d71cebd8384d2221
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-04-17 23:35:26 +0300

    Add follow button

---
+++ apps/web/src/pages/user/components/FollowButton.tsx
@@ -0,0 +1,30 @@
+import { PlusIcon } from '@heroicons/react/24/outline';
+import { Spinner } from '../../../components';
+
+export interface FollowButtonProps {
+  isFollowing: boolean;
+  loading: boolean;
+  onClick: () => Promise<void>;
+}
+
+function FollowButton({ isFollowing, loading, onClick }: FollowButtonProps): JSX.Element {
+  const followText = isFollowing ? 'Following' : 'Follow';
+  const Icon = isFollowing ? <></> : <PlusIcon className="h-5 w-5 text-white" />;
+
+  return (
+    <button
+      className="flex items-center rounded bg-midnight px-4 py-2 text-white transition ease-in-out hover:bg-midnight/90"
+      onClick={onClick}
+    >
+      {!loading && (
+        <>
+          {Icon}
+          <span className="ml-2">{followText}</span>
+        </>
+      )}
+      {loading && <Spinner />}
+    </button>
+  );
+}
+
+export default FollowButton;
