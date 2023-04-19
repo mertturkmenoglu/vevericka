4c6e4260fdc695f396367f8272371f2eae7f5f44
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-04-19 14:36:31 +0300

    Add link preview to post content

--- apps/web/src/lib/post/content.tsx
+++ apps/web/src/lib/post/content.tsx
@@ -1,9 +1,29 @@
-import { detectHashtags, detectUsernames, linkify } from './post-utils';
+import { useLazyQuery } from '@apollo/client';
+import { detectHashtags, detectUsernames, getFirstUrl, linkify } from './post-utils';
 import { getYoutubeIframe } from './youtube-iframe';
+import { linkPreviewQueryDocument } from '../../graphql/queries/linkPreviewQuery';
+import { useEffect } from 'react';
+import { LinkIcon } from '@heroicons/react/24/outline';
 
 export const getPostContent = (text: string): JSX.Element => {
   const content = linkify(detectUsernames(detectHashtags(text)));
   const youtubeIframe = getYoutubeIframe(content);
+  const firstUrl = getFirstUrl(text);
+
+  const [getMeta, { data, loading, error }] = useLazyQuery(linkPreviewQueryDocument);
+
+  useEffect(() => {
+    if (firstUrl) {
+      getMeta({
+        variables: {
+          url: firstUrl,
+        },
+      });
+    }
+  }, [firstUrl]);
+
+  const shouldShowLinkPreview = firstUrl && !error && data && !loading && !youtubeIframe;
+
   return (
     <>
       <div
@@ -11,6 +31,42 @@ export const getPostContent = (text: string): JSX.Element => {
         className="text-sm"
       />
       {youtubeIframe}
+      {shouldShowLinkPreview && (
+        <a
+          href={data.linkPreview.url ?? ''}
+          className="mt-4 block rounded bg-neutral-100"
+        >
+          {data.linkPreview.image && (
+            <img
+              src={data.linkPreview.image}
+              className="rounded-t"
+            />
+          )}
+
+          {!data.linkPreview.image && (
+            <div className="flex h-32 items-center justify-center rounded-t bg-neutral-200">
+              <LinkIcon className="h-5 w-5 text-midnight" />
+            </div>
+          )}
+
+          <div
+            className="mt-2 px-2 font-medium"
+            style={{
+              fontFamily: 'Jost',
+            }}
+          >
+            {data.linkPreview.title}
+          </div>
+          <div
+            className="px-2 pb-2 text-xs font-light"
+            style={{
+              fontFamily: 'Jost',
+            }}
+          >
+            {data.linkPreview.description ?? data.linkPreview.url}
+          </div>
+        </a>
+      )}
     </>
   );
 };
