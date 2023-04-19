6e3500ec0560396bce8c9c1754819d7c6482b570
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-04-19 21:18:53 +0300

    Update Apollo cache type policy

--- apps/web/src/apollo.ts
+++ apps/web/src/apollo.ts
@@ -2,6 +2,24 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
   uri: import.meta.env.VITE_BASE_URL + '/graphql',
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          feed: {
+            keyArgs: false,
+            merge(existing, incoming) {
+              const existingData =
+                !existing || typeof existing.posts[Symbol.iterator] !== 'function' ? { posts: [] } : existing;
+              const newArr = [...existingData.posts, ...incoming.posts];
+              return {
+                posts: newArr,
+              };
+            },
+          },
+        },
+      },
+    },
+  }),
   credentials: 'include',
 });
