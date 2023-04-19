2808d647104bba29613eedd411ce90c1aae2b3c7
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-04-19 18:51:22 +0300

    Add about section to user detail page

--- apps/web/src/pages/user/User.tsx
+++ apps/web/src/pages/user/User.tsx
@@ -4,15 +4,19 @@ import { GetProfileDataQuery } from '../../generated/graphql';
 import { PostCard } from '../../components';
 import { useFragment } from '../../generated';
 import { postFragmentDocument, profileFragmentDocument } from '../../graphql';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Banner from './components/Banner';
 import UserPrimaryInfo from './components/UserPrimaryInfo';
 import UserActions from './components/UserActions';
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/outline';
+import About from './components/About';
+import * as Separator from '@radix-ui/react-separator';
 
 function User(): JSX.Element {
   const data = useLoaderData() as GetProfileDataQuery;
   const user = useFragment(profileFragmentDocument, data.profile);
   const posts = useFragment(postFragmentDocument, data.posts);
+  const [showAbout, setShowAbout] = useState(false);
 
   useEffect(() => {
     const el = document.getElementById('app-bar-page-name');
@@ -36,6 +40,28 @@ function User(): JSX.Element {
             <UserActions user={user} />
           </div>
         </div>
+
+        <div className="mt-4">
+          <button
+            className="-ml-2 flex items-center justify-center space-x-2 rounded px-2 py-2 transition duration-200 ease-in-out hover:bg-midnight/10"
+            onClick={() => setShowAbout((prev) => !prev)}
+          >
+            <span className="text-lg font-medium">About</span>
+            {showAbout ? (
+              <ChevronUpIcon className="h-5 w-5 text-midnight" />
+            ) : (
+              <ChevronDownIcon className="h-5 w-5 text-midnight" />
+            )}
+          </button>
+
+          {showAbout && <About user={user} />}
+        </div>
+
+        <Separator.Root
+          className="mt-2 h-[1px] w-full bg-neutral-200"
+          decorative
+          orientation="horizontal"
+        />
 
         <div className="mx-auto mt-8 flex w-1/2">
           <div className="w-full divide-y-2 divide-neutral-100 [&>*]:py-2">
---
+++ apps/web/src/pages/user/components/About.tsx
@@ -0,0 +1,71 @@
+import { ProfileFragmentFragment } from '../../../generated/graphql';
+
+export interface AboutProps {
+  user: ProfileFragmentFragment;
+}
+
+export interface AboutItemProps {
+  title: string;
+  content?: string | null | undefined;
+  link?: boolean;
+}
+
+function AboutItem({ title, content = '', link = false }: AboutItemProps): JSX.Element {
+  if (!content) {
+    return <></>;
+  }
+
+  return (
+    <div className="flex flex-col space-y-2">
+      <span className="text-sm text-neutral-400">{title}</span>
+      {link && (
+        <a
+          href={content ?? '#'}
+          className="block text-lg font-medium text-berry hover:underline"
+        >
+          {content}
+        </a>
+      )}
+      {!link && <span className="text-lg font-medium">{content}</span>}
+    </div>
+  );
+}
+
+function About({ user }: AboutProps): JSX.Element {
+  return (
+    <div className="mt-4 grid grid-cols-3 gap-y-4">
+      <AboutItem
+        title="Job"
+        content={user.job}
+      />
+
+      <AboutItem
+        title="School"
+        content={user.school}
+      />
+
+      <AboutItem
+        title="Bio"
+        content={user.description}
+      />
+
+      <AboutItem
+        title="Location"
+        content={user.country}
+      />
+
+      <AboutItem
+        title="Joined"
+        content={new Date(user.createdAt).toLocaleDateString()}
+      />
+
+      <AboutItem
+        title="Website"
+        link
+        content={user.website}
+      />
+    </div>
+  );
+}
+
+export default About;
