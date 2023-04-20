a8604d660086384f0cd2f984f52e2a6d67d1a5a2
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-04-20 12:02:59 +0300

    Add context name to allow multiple loaders

--- apps/web/src/hooks/useUploadcare/useUploadcare.tsx
+++ apps/web/src/hooks/useUploadcare/useUploadcare.tsx
@@ -6,10 +6,11 @@ const STYLES = 'https://unpkg.com/@uploadcare/blocks@0.17.1/web/file-uploader-re
 
 export interface UploadcareOptions {
   id: string;
+  contextName: string;
   onDataOutput: (event: Event) => void;
 }
 
-export function useUploadcare({ id, onDataOutput }: UploadcareOptions) {
+export function useUploadcare({ id, contextName, onDataOutput }: UploadcareOptions) {
   useEffect(() => {
     const fn = async () => {
       const blocks = await connectBlocksFrom('https://unpkg.com/@uploadcare/blocks@0.17.1/web/blocks-browser.min.js');
@@ -27,17 +28,27 @@ export function useUploadcare({ id, onDataOutput }: UploadcareOptions) {
       // Now you can add uploader using native DOM API methods
       const uploader = new blocks.FileUploaderRegular();
       uploader.setAttribute('css-src', STYLES);
+      uploader.setAttribute('ctx-name', contextName);
       uploader.classList.add('my-config-class');
 
       document.getElementById(id)?.appendChild(uploader);
     };
 
+    const onOutput = (event: Event) => {
+      const e = event as CustomEvent;
+      const { ctx } = e.detail;
+
+      if (ctx === contextName) {
+        onDataOutput(event);
+      }
+    };
+
     fn();
 
-    window.addEventListener('LR_DATA_OUTPUT', onDataOutput);
+    window.addEventListener('LR_DATA_OUTPUT', onOutput);
 
     return () => {
-      window.removeEventListener('LR_DATA_OUTPUT', onDataOutput);
+      window.removeEventListener('LR_DATA_OUTPUT', onOutput);
     };
   }, []);
 }
