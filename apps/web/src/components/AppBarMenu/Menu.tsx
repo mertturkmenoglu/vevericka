cd39fbdaa551f46f68fe6d694f6b996395bb67b9
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-04-22 16:54:36 +0300

    Use extracted Transition component in AppBar Menu component

--- apps/web/src/components/AppBarMenu/Menu.tsx
+++ apps/web/src/components/AppBarMenu/Menu.tsx
@@ -1,11 +1,12 @@
-import React, { Fragment } from 'react';
+import { Menu } from '@headlessui/react';
 import clsx from 'clsx';
-import { Menu, Transition } from '@headlessui/react';
-import MenuItems from './MenuItems';
-import MenuButton from './MenuButton';
-import { useAuth } from '../../hooks';
+import React from 'react';
 import { useFragment } from '../../generated';
 import { userFragmentDocument } from '../../graphql';
+import { useAuth } from '../../hooks';
+import { Transition } from '../Transition';
+import MenuButton from './MenuButton';
+import MenuItems from './MenuItems';
 
 export interface AppBarMenuProps {
   className?: string;
@@ -26,15 +27,7 @@ function AppBarMenu({ className }: AppBarMenuProps): JSX.Element {
         className="relative inline-block w-min text-left"
       >
         <MenuButton src={me.image} />
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
+        <Transition>
           <Menu.Items
             static
             className="absolute right-0 z-50 mt-4 w-48 origin-top-right rounded-md bg-white shadow-md ring-1 ring-black ring-opacity-5 focus:outline-none"
