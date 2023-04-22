d6a3fe46d5d5446d7fd80e1c10a760e2ac6717e1
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-04-22 17:00:23 +0300

    Fix warnings

--- apps/web/src/components/LandingAppBar/LandingAppBar.tsx
+++ apps/web/src/components/LandingAppBar/LandingAppBar.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
-import { ReactComponent as SquirrelLogo } from '../../assets/squirrel.svg';
 import { Link } from 'react-router-dom';
+import { ReactComponent as SquirrelLogo } from '../../assets/squirrel.svg';
 
 export interface LandingAppBarProps {
   className?: string;
@@ -10,7 +10,7 @@ function LandingAppBar({ className }: LandingAppBarProps): JSX.Element {
   return (
     <nav
       className={clsx(
-        'container mx-auto flex items-center justify-between rounded-full bg-zinc-100 py-2 px-8',
+        'container mx-auto flex items-center justify-between rounded-full bg-zinc-100 px-8 py-2',
         className
       )}
     >
@@ -24,7 +24,7 @@ function LandingAppBar({ className }: LandingAppBarProps): JSX.Element {
       <div className="flex items-center space-x-4">
         <Link
           to="/login"
-          className="rounded-full bg-midnight py-1.5 px-4 text-sm font-medium text-white"
+          className="rounded-full bg-midnight px-4 py-1.5 text-sm font-medium text-white"
         >
           Get Started
         </Link>
