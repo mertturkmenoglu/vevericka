48aa3ec24f6d3c49289797e94ac10ec28083c996
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-04-22 17:49:00 +0300

    Extract NoSearchResults component

---
+++ apps/web/src/components/NoSearchResults/NoSearchResults.tsx
@@ -0,0 +1,10 @@
+function NoSearchResults(): JSX.Element {
+  return (
+    <div className="mx-auto mt-8 text-center text-lg font-medium">
+      <div>We couldn't find any results</div>
+      <div>Try changing your words</div>
+    </div>
+  );
+}
+
+export default NoSearchResults;
