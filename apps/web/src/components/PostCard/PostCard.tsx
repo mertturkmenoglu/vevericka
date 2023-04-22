78cda0efa676979cf19303c12fecc0932c7ac015
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-04-22 16:37:53 +0300

    Extract Title component

--- apps/web/src/components/PostCard/PostCard.tsx
+++ apps/web/src/components/PostCard/PostCard.tsx
@@ -1,17 +1,12 @@
-import { useApolloClient, useMutation } from '@apollo/client';
-import { ArrowDownIcon, ArrowUpIcon, ArrowUpTrayIcon, ChatBubbleBottomCenterIcon } from '@heroicons/react/24/outline';
 import { Link } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import { FragmentType, useFragment } from '../../generated';
-import { countFragmentDocument, postFragmentDocument, userFragmentDocument, votePostDocument } from '../../graphql';
-import { copyToClipboard } from '../../lib';
+import { countFragmentDocument, postFragmentDocument, userFragmentDocument } from '../../graphql';
 import { LazyImage } from '../LazyImage';
-import ActionButton from './ActionButton';
-import { useFormattedDate } from './hooks';
+import Actions from './Actions';
 import MoreMenu from './MoreMenu';
 import PostContent from './PostContent';
 import PostImages from './PostImages';
+import Title from './Title';
 
 export interface PostCardProps {
   post: FragmentType<typeof postFragmentDocument>;
@@ -21,49 +16,7 @@ function PostCard(props: PostCardProps): JSX.Element {
   const post = useFragment(postFragmentDocument, props.post);
   const user = useFragment(userFragmentDocument, post.user);
   const count = useFragment(countFragmentDocument, post._count);
-
-  const [vote] = useMutation(votePostDocument);
-  const client = useApolloClient();
-
-  const formattedDate = useFormattedDate(post.createdAt);
-
   const hasMedia = post.images.length > 0 || post.videos.length > 0;
-
-  const onVote = async (value: string) => {
-    const oldVote = post.vote;
-    let newVote = 'none';
-
-    if (value === 'like') {
-      newVote = post.vote === 'like' ? 'none' : 'like';
-    } else {
-      newVote = post.vote === 'dislike' ? 'none' : 'dislike';
-    }
-
-    client.cache.modify({
-      id: post.id,
-      fields: {
-        vote: () => newVote,
-      },
-    });
-
-    const res = await vote({
-      variables: {
-        id: post.id,
-        vote: newVote,
-      },
-    });
-
-    if (res.errors) {
-      toast('Something went wrong', { type: 'error', autoClose: 3000 });
-    } else {
-      client.cache.modify({
-        id: post.id,
-        fields: {
-          vote: () => oldVote,
-        },
-      });
-    }
-  };
 
   return (
     <article
@@ -86,20 +39,12 @@ function PostCard(props: PostCardProps): JSX.Element {
       </Link>
 
       <div className="w-10/12">
-        <div className="flex items-baseline space-x-1">
-          <Link
-            to={`/u/${user.id}`}
-            className="text-sm font-semibold tracking-wide text-midnight hover:underline"
-          >
-            {user.name}
-          </Link>
-          <Link
-            to={`/p/${post.id}`}
-            className="text-xs tracking-tighter text-neutral-600 hover:underline"
-          >
-            {formattedDate}
-          </Link>
-        </div>
+        <Title
+          userId={user.id}
+          name={user.name}
+          postId={post.id}
+          createdAt={post.createdAt}
+        />
 
         <div className="text-xs font-light tracking-tighter sm:text-base sm:tracking-normal">
           <PostContent
@@ -110,36 +55,10 @@ function PostCard(props: PostCardProps): JSX.Element {
 
         <PostImages images={post.images} />
 
-        <div className="-ml-2 mt-1 flex items-center justify-between">
-          <ActionButton
-            icon={ArrowUpIcon}
-            text={count.likes}
-            state={post.vote === 'like' ? 'active' : 'inactive'}
-            onClick={async () => onVote('like')}
-          />
-
-          <ActionButton
-            icon={ArrowDownIcon}
-            text={count.dislikes}
-            state={post.vote === 'dislike' ? 'active' : 'inactive'}
-            onClick={async () => onVote('dislike')}
-          />
-
-          <ActionButton
-            icon={ChatBubbleBottomCenterIcon}
-            text={count.comments}
-          />
-
-          <ActionButton
-            icon={ArrowUpTrayIcon}
-            text={''}
-            onClick={() => {
-              const url = window.location.origin + `/p/${post.id}`;
-              copyToClipboard(url);
-              toast('Copied to clipboard', { type: 'success', autoClose: 3000 });
-            }}
-          />
-        </div>
+        <Actions
+          post={post}
+          count={count}
+        />
       </div>
 
       <div className="w-1/12">
---
+++ apps/web/src/components/PostCard/Title.tsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import { useFormattedDate } from './hooks';
+
+export interface TitleProps {
+  userId: string;
+  name: string;
+  postId: string;
+  createdAt: string;
+}
+
+function Title({ userId, name, postId, createdAt }: TitleProps): JSX.Element {
+  const formattedDate = useFormattedDate(createdAt);
+
+  return (
+    <div className="flex items-baseline space-x-1">
+      <Link
+        to={`/u/${userId}`}
+        className="text-sm font-semibold tracking-wide text-midnight hover:underline"
+      >
+        {name}
+      </Link>
+      <Link
+        to={`/p/${postId}`}
+        className="text-xs tracking-tighter text-neutral-600 hover:underline"
+      >
+        {formattedDate}
+      </Link>
+    </div>
+  );
+}
+
+export default Title;
