d7b07a4391bbd2be730d4acdc1a456848f27c2a8
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-04-24 19:19:32 +0300

    Update Messages page dark theme styles

--- apps/web/src/pages/messages/Messages.tsx
+++ apps/web/src/pages/messages/Messages.tsx
@@ -8,9 +8,9 @@ function Messages(): JSX.Element {
         <Helmet>
           <title>Messages | Vevericka</title>
         </Helmet>
-        <h2 className="text-2xl font-medium">Messages</h2>
-        <hr className="h-[1px] max-w-md border border-midnight bg-midnight" />
-        <div className="mt-4">Messages</div>
+        <h2 className="text-2xl font-medium text-midnight dark:text-white">Messages</h2>
+        <hr className="h-[1px] max-w-md border border-midnight bg-midnight dark:border-white dark:bg-white" />
+        <div className="mt-4 text-midnight dark:text-white">Messages</div>
       </div>
     </MainLayout>
   );
