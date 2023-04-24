8594d2d5b42ed36cca405dc1faa91c312db53b4e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-04-24 18:34:24 +0300

    Update DotsMenu dark theme styles

--- apps/web/src/components/DotsMenu/DotsMenu.tsx
+++ apps/web/src/components/DotsMenu/DotsMenu.tsx
@@ -14,12 +14,12 @@ function DotsMenu({ children }: DotsMenuProps): JSX.Element {
       className="relative inline-block w-min text-left"
     >
       <Menu.Button className="group flex items-center space-x-2 rounded-full px-2 py-2 transition ease-in-out hover:bg-berry/10">
-        <EllipsisVerticalIcon className="h-5 w-5 text-midnight group-hover:text-berry" />
+        <EllipsisVerticalIcon className="h-5 w-5 text-midnight group-hover:text-berry dark:text-white" />
       </Menu.Button>
       <Transition>
         <Menu.Items
           static
-          className="absolute right-0 z-50 mt-2 flex w-40 origin-top-right flex-col space-y-2 rounded bg-white p-2 shadow ring-1 ring-black ring-opacity-5 focus:outline-none"
+          className="absolute right-0 z-50 mt-2 flex w-40 origin-top-right flex-col space-y-2 rounded bg-white p-2 shadow ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-neutral-800"
         >
           {children}
         </Menu.Items>
