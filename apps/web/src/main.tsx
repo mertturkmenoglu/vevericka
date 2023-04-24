b8973d0bf1d1453e7adebd47648f4350b4fea9db
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-04-24 18:33:06 +0300

    Add ThemeProvider to app root

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,19 +1,22 @@
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 
 import { RouterProvider } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/client';
-import { client as apolloClient } from './apollo';
-import { router } from './router';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { client as apolloClient } from './apollo';
+import { ThemeProvider } from './components';
+import './index.css';
+import { router } from './router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ApolloProvider client={apolloClient}>
-      <RouterProvider router={router} />
-      <ToastContainer />
+      <ThemeProvider>
+        <RouterProvider router={router} />
+        <ToastContainer />
+      </ThemeProvider>
     </ApolloProvider>
   </React.StrictMode>
 );
