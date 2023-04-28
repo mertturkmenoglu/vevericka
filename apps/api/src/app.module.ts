79c223522debb96585fc53fad07bd760cb67aff8
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-04-28 20:54:46 +0300

    Remove CORS from GraphQL module registration

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,16 +1,16 @@
-import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
-import { BullModule } from "@nestjs/bull";
-import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-import { GraphQLModule } from "@nestjs/graphql";
-import { AuthModule } from "./auth/auth.module";
-import { BookmarksModule } from "./bookmarks/bookmarks.module";
-import { ExploreModule } from "./explore/explore.module";
-import { FeedModule } from "./feed/feed.module";
-import { LinkModule } from "./link/link-module";
-import { PostsModule } from "./posts/posts.module";
-import { SearchModule } from "./search/search.module";
-import { UsersModule } from "./users/users.module";
+import {ApolloDriver, ApolloDriverConfig} from "@nestjs/apollo";
+import {BullModule} from "@nestjs/bull";
+import {Module} from "@nestjs/common";
+import {ConfigModule} from "@nestjs/config";
+import {GraphQLModule} from "@nestjs/graphql";
+import {AuthModule} from "./auth/auth.module";
+import {BookmarksModule} from "./bookmarks/bookmarks.module";
+import {ExploreModule} from "./explore/explore.module";
+import {FeedModule} from "./feed/feed.module";
+import {LinkModule} from "./link/link-module";
+import {PostsModule} from "./posts/posts.module";
+import {SearchModule} from "./search/search.module";
+import {UsersModule} from "./users/users.module";
 
 @Module({
   imports: [
@@ -35,14 +35,6 @@ import { UsersModule } from "./users/users.module";
       playground: true,
       introspection: true,
       stopOnTerminationSignals: false,
-      debug: true,
-      cors: {
-        origin: "http://localhost:5173",
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-        preflightContinue: false,
-        optionsSuccessStatus: 204,
-        credentials: true,
-      },
     }),
     BullModule.forRoot({
       redis: {
