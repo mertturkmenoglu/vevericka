32232ad3f5d29d1f5c38337f511c8aa830ca6a1a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-05-02 12:47:59 +0300

    Refactor feed resolver imports

--- apps/api/src/feed/feed.resolver.ts
+++ apps/api/src/feed/feed.resolver.ts
@@ -1,9 +1,9 @@
 import { NotFoundException, UseGuards } from "@nestjs/common";
 import { Args, Query, Resolver } from "@nestjs/graphql";
-import { JwtAuthGuard } from "src/auth/guards";
-import { CurrentUser as CurrentUserDecorator } from "src/common/decorators/current-user.decorator";
-import { CurrentUser } from "src/common/types/current-user.type";
+import { JwtAuthGuard } from "../auth/guards";
 import { PaginationArgs } from "../common/args/pagination.args";
+import { CurrentUser as CurrentUserDecorator } from "../common/decorators/current-user.decorator";
+import { CurrentUser } from "../common/types/current-user.type";
 import { FeedService } from "./feed.service";
 import { Feed } from "./models/feed.model";
 
