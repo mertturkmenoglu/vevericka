7cc2b067e81cde808e9c403ba3f0534e47753ed9
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-05-06 19:35:55 +0300

    Add Next Auth to helin

--- apps/helin/src/app/layout.tsx
+++ apps/helin/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { NextAuthProvider } from "@/app/providers";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -15,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NextAuthProvider>{children}</NextAuthProvider>
+      </body>
     </html>
   );
 }
--- apps/helin/src/app/page.tsx
+++ apps/helin/src/app/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Helin
+      <button onClick={() => signIn()}>Sign in</button>
     </main>
   );
 }
