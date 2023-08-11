558295e40900b32ee6503efaceda2a68b9726fc6
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-08-12 00:32:20 +0300

    Use db service in example module

--- apps/api/src/example/example.module.ts
+++ apps/api/src/example/example.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ExampleResolver } from './example.resolver';
+import { DbModule } from '@/db/db.module';
 
 @Module({
+  imports: [DbModule],
   providers: [ExampleResolver],
 })
 export class ExampleModule {}
