3a6b2cd67ac704aa01a36ca6b249240bca1c453b
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-08-12 17:08:20 +0300

    reformat

--- apps/api/src/common/models/count.model.ts
+++ apps/api/src/common/models/count.model.ts
@@ -1,6 +1,6 @@
-import { Field, ObjectType } from "@nestjs/graphql";
+import { Field, ObjectType } from '@nestjs/graphql';
 
-@ObjectType({ description: "Count" })
+@ObjectType({ description: 'Count' })
 export class Count {
   @Field()
   dislikes!: number;
--- apps/api/src/common/scalars/date.scalar.ts
+++ apps/api/src/common/scalars/date.scalar.ts
@@ -1,20 +1,23 @@
-import { CustomScalar, Scalar } from "@nestjs/graphql";
-import { Kind, ValueNode } from "graphql";
+import { CustomScalar, Scalar } from '@nestjs/graphql';
+import { Kind, ValueNode } from 'graphql';
 
-@Scalar("Date", (type) => Date)
+@Scalar('Date', () => Date)
 export class DateScalar implements CustomScalar<number, Date> {
-  description = "Date custom scalar type";
+  description = 'Date custom scalar type';
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   parseValue(value: number): Date {
     return new Date(value); // value from the client
   }
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   serialize(value: Date): number {
     return value.getTime(); // value sent to the client
   }
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   parseLiteral(ast: ValueNode): Date | null {
     if (ast.kind === Kind.INT) {
