caf834e4fe4a72a925394c6c83112a7c4d965bd7
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-08-12 20:02:13 +0300

    create migration

---
+++ apps/api/src/db/drizzle/0005_late_captain_midlands.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS "follows" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"following_id" uuid NOT NULL,
+	"follower_id" uuid NOT NULL
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "follows" ADD CONSTRAINT "follows_following_id_users_id_fk" FOREIGN KEY ("following_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "follows" ADD CONSTRAINT "follows_follower_id_users_id_fk" FOREIGN KEY ("follower_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
