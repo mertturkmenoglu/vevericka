88976e4dc67e14247cebf3b743b8215fc0cb1d86
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-08-12 15:25:29 +0300

    never gonna

--- apps/apiold/src/link/link-module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { LinkResolver } from "./link.resolver";
-import { LinkService } from "./link.service";
-
-@Module({
-  providers: [LinkResolver, LinkService],
-})
-export class LinkModule {}
