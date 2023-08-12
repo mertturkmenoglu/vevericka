d5dc02f5c592d1849cc6f4cbbd806e2189895052
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-08-12 17:49:12 +0300

    remove old api

--- apps/apiold/src/search/models/post/post-hit.model.ts
+++
@@ -1,9 +0,0 @@
-import { Field, ObjectType } from "@nestjs/graphql";
-import { Post } from "../../../posts/models/post.model";
-import { Hit } from "../common/hit.model";
-
-@ObjectType()
-export class PostHit extends Hit {
-  @Field(() => Post)
-  _source!: Post;
-}
