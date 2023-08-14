190172574153abbf577d65470dc4b28f54970f5b
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-08-14 15:18:52 +0300

    update profile model

--- apps/api/src/users/models/profile.model.ts
+++ apps/api/src/users/models/profile.model.ts
@@ -13,10 +13,10 @@ export class UserCount {
   posts!: number;
 }
 
-@ObjectType({ description: 'user' })
-export class Profile extends User {
-  @Field(() => UserCount)
-  count!: UserCount;
+@ObjectType({ description: 'ProfileMeta' })
+export class ProfileMeta {
+  @Field()
+  isMe!: boolean;
 
   @Field()
   isFollowing!: boolean;
@@ -24,6 +24,90 @@ export class Profile extends User {
   @Field()
   hasPendingFollowRequest!: boolean;
 
+  @Field(() => UserCount)
+  count!: UserCount;
+}
+
+@ObjectType({ description: 'ProfileDescriptionTag' })
+export class ProfileDescriptionTag {
   @Field()
-  isMe!: boolean;
+  id!: string;
+
+  @Field()
+  userId!: string;
+
+  @Field()
+  tagId!: string;
+
+  @Field()
+  start!: number;
+
+  @Field()
+  end!: number;
+}
+
+@ObjectType({ description: 'ProfileDescriptionUrl' })
+export class ProfileDescriptionUrl {
+  @Field()
+  id!: string;
+
+  @Field()
+  userId!: string;
+
+  @Field()
+  start!: number;
+
+  @Field()
+  end!: number;
+
+  @Field(() => String)
+  meta!: unknown;
+
+  @Field()
+  url!: string;
+}
+
+@ObjectType({ description: 'ProfileDescriptionMention' })
+export class ProfileDescriptionMention {
+  @Field()
+  id!: string;
+
+  @Field()
+  userId!: string;
+
+  @Field()
+  start!: number;
+
+  @Field()
+  end!: number;
+
+  @Field()
+  mention!: string;
+
+  @Field(() => String, { nullable: true })
+  mentionedUserId!: string | null;
+}
+
+@ObjectType({ description: 'ProfileDescription' })
+export class ProfileDescription {
+  @Field(() => String, { nullable: true })
+  description!: string | null;
+
+  @Field(() => [ProfileDescriptionTag])
+  tags!: ProfileDescriptionTag[];
+
+  @Field(() => [ProfileDescriptionUrl])
+  urls!: ProfileDescriptionUrl[];
+
+  @Field(() => [ProfileDescriptionMention])
+  mentions!: ProfileDescriptionMention[];
+}
+
+@ObjectType({ description: 'user' })
+export class Profile extends User {
+  @Field(() => ProfileDescription)
+  descriptionMeta!: ProfileDescription;
+
+  @Field(() => ProfileMeta)
+  meta!: ProfileMeta;
 }
