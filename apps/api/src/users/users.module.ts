0bc5cf79502fc84285d29fda1d193cc4fe7b93e3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-08-14 15:12:24 +0300

    add users repository as a provider to the module declaration

--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
 import { DbModule } from '@/db/db.module';
+import { UsersRepository } from '@/users/users.repository';
 
 @Module({
-  providers: [UsersResolver, UsersService],
+  providers: [UsersResolver, UsersService, UsersRepository],
   imports: [DbModule],
 })
 export class UsersModule {}
