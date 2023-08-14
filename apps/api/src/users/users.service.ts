fc4c258b60f41c58f1303ab3ac70f104d7fc1670
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-08-14 15:18:59 +0300

    update users service

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -2,10 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { DbService } from '@/db/db.service';
 import { TUser, users } from '@/db';
 import { eq } from 'drizzle-orm';
+import { Profile } from '@/users/models/profile.model';
+import { UsersRepository } from '@/users/users.repository';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly db: DbService) {}
+  constructor(
+    private readonly db: DbService,
+    private readonly repository: UsersRepository,
+  ) {}
 
   async findOneById(id: string): Promise<TUser | null> {
     const results = await this.db.client
@@ -19,6 +24,13 @@ export class UsersService {
     }
 
     return results[0];
+  }
+
+  async findOneProfileById(
+    thisUserId: string,
+    id: string,
+  ): Promise<Profile | null> {
+    return await this.repository.findOneProfileById(thisUserId, id);
   }
 
   async findAll(): Promise<TUser[]> {
