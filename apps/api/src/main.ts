a58f2b25b435f05ec57ef7a35b66a15330e55e1b
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-08-15 17:06:57 +0300

    add cookie parser

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,8 @@ async function bootstrap() {
   });
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.use(cookieParser());
 
   const PORT = process.env['PORT'] ?? 3000;
   await app.listen(PORT);
