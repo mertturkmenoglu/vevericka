e4262737da9179102eac2ab9b06d241d11a46ff3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-08-16 20:46:13 +0300

    update bookmarks table

--- apps/api/src/db/tables/bookmarks.ts
+++ apps/api/src/db/tables/bookmarks.ts
@@ -1,22 +1,28 @@
-import { pgTable, timestamp, uuid } from 'drizzle-orm/pg-core';
+import { index, pgTable, timestamp, uuid } from 'drizzle-orm/pg-core';
 import { users } from '@/db/tables/users';
 import { posts } from '@/db/tables/posts';
 import { InferModel } from 'drizzle-orm';
 
-export const bookmarks = pgTable('bookmarks', {
-  id: uuid('id').primaryKey().defaultRandom(),
-  userId: uuid('user_id')
-    .references(() => users.id)
-    .notNull(),
-  postId: uuid('post_id')
-    .references(() => posts.id)
-    .notNull(),
-  createdAt: timestamp('created_at', { withTimezone: true })
-    .notNull()
-    .defaultNow(),
-  updatedAt: timestamp('updated_at', { withTimezone: true })
-    .notNull()
-    .defaultNow(),
-});
+export const bookmarks = pgTable(
+  'bookmarks',
+  {
+    id: uuid('id').primaryKey().defaultRandom(),
+    userId: uuid('user_id')
+      .references(() => users.id, { onDelete: 'cascade' })
+      .notNull(),
+    postId: uuid('post_id').references(() => posts.id),
+    createdAt: timestamp('created_at', { withTimezone: true })
+      .notNull()
+      .defaultNow(),
+    updatedAt: timestamp('updated_at', { withTimezone: true })
+      .notNull()
+      .defaultNow(),
+  },
+  (t) => {
+    return {
+      userIdx: index('user_idx').on(t.userId),
+    };
+  },
+);
 
 export type TBookmark = InferModel<typeof bookmarks>;
