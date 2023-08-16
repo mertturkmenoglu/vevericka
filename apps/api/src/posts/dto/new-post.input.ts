8ecbf801e988757a195aec633ca08ac08256cc54
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-08-16 16:38:28 +0300

    update new post input

--- apps/api/src/posts/dto/new-post.input.ts
+++ apps/api/src/posts/dto/new-post.input.ts
@@ -3,13 +3,75 @@ import { MaxLength } from 'class-validator';
 
 @InputType()
 export class NewPostInput {
-  @Field()
+  @Field(() => String, { nullable: true })
   @MaxLength(255)
-  content!: string;
+  content!: string | null;
 
-  @Field(() => [String])
-  imageUrls!: string[];
+  @Field(() => String, { nullable: true })
+  @MaxLength(32)
+  source!: string | null;
 
-  @Field(() => [String])
-  videoUrls!: string[];
+  @Field(() => String, { nullable: true })
+  @MaxLength(32)
+  location!: string | null;
+
+  @Field(() => Boolean, { nullable: true })
+  sensitive!: boolean | null;
+
+  @Field(() => String, { nullable: true })
+  referenceId!: string | null;
+
+  @Field(() => String, { nullable: true })
+  referenceType!: string | null;
+
+  @Field(() => String, { nullable: true })
+  replySetting!: string | null;
+
+  @Field(() => [NewPostAttachmentInput])
+  attachments!: NewPostAttachmentInput[];
+
+  @Field(() => NewPostPollInput, { nullable: true })
+  poll!: NewPostPollInput | null;
+}
+
+@InputType()
+export class NewPostAttachmentInput {
+  @Field()
+  type!: string;
+
+  @Field()
+  url!: string;
+
+  @Field(() => Number, { nullable: true })
+  width!: number | null;
+
+  @Field(() => Number, { nullable: true })
+  height!: number | null;
+
+  @Field(() => Number, { nullable: true })
+  duration!: number | null;
+
+  @Field()
+  order!: number;
+}
+
+@InputType()
+export class NewPostPollInput {
+  @Field()
+  start!: Date;
+
+  @Field()
+  end!: Date;
+
+  @Field(() => [NewPostPollOptionInput])
+  options!: NewPostPollOptionInput[];
+}
+
+@InputType()
+export class NewPostPollOptionInput {
+  @Field()
+  option!: string;
+
+  @Field()
+  order!: number;
 }
