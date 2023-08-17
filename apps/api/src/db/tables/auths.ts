3452cd649e9030c58c305239b29d7801bcd0de19
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-08-17 03:33:12 +0300

    update auth table

--- apps/api/src/db/tables/auths.ts
+++ apps/api/src/db/tables/auths.ts
@@ -1,4 +1,11 @@
-import { pgEnum, pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+import {
+  boolean,
+  pgEnum,
+  pgTable,
+  timestamp,
+  uuid,
+  varchar,
+} from 'drizzle-orm/pg-core';
 import { InferModel } from 'drizzle-orm';
 
 export const authProvidersEnum = pgEnum('auth_providers', [
@@ -11,6 +18,9 @@ export const auths = pgTable('auths', {
   id: uuid('id').primaryKey().defaultRandom(),
   sub: varchar('sub', { length: 256 }).notNull(),
   type: authProvidersEnum('type').notNull(),
+  accountSetupCompleted: boolean('account_setup_completed')
+    .notNull()
+    .default(false),
   createdAt: timestamp('created_at', { withTimezone: true })
     .notNull()
     .defaultNow(),
